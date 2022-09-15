73bb1a9e5ce50c1f3455036ef57865c409cc356f
Niols <niols@niols.fr>
2022-09-15 15:26:59 +0200

    Add plutonomicon cache

--- configuration.nix
+++ configuration.nix
@@ -278,6 +278,7 @@
       "https://hydra.iohk.io"
       "https://iohk.cachix.org"
       "https://hachi.cachix.org"
+      "https://public-plutonomicon.cachix.org"
     ];
     settings.trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
@@ -285,6 +286,7 @@
       "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
       "iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo="
       "hachi.cachix.org-1:LhKjNU6sYCnwfR9NH48iI+Pug9D+x+TsHy28poUqINI="
+      "plutonomicon.cachix.org-1:3AKJMhCLn32gri1drGuaZmFrmnue+KkKrhhubQk/CWc="
     ];
 
     # ## Garbage-collect automatically on a weekly basis; keep old
