9601d6faccc87ed619fc21d63e77c298a5859c71
Niols <niols@niols.fr>
2022-09-22 18:38:45 +0200

    All follow the same nixpkgs

--- flake.nix
+++ flake.nix
@@ -11,10 +11,13 @@
 
     emacs-overlay = {
       url = "github:nix-community/emacs-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     nix-doom-emacs = {
       url = "github:nix-community/nix-doom-emacs";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.emacs-overlay.follows = "emacs-overlay";
     };
   };
 
