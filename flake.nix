5b2e46f69f9392c4dcd3879ea19a680db336b0be
Niols <niols@niols.fr>
2022-09-23 14:18:07 +0200

    Comment HM options

--- flake.nix
+++ flake.nix
@@ -32,9 +32,28 @@
 
         home-manager.nixosModules.home-manager {
           home-manager = {
+            ## By default, Home Manager uses a private pkgs instance that is
+            ## configured via the `home-manager.users.<name>.nixpkgs` options.
+            ## The following option instead uses the global pkgs that is
+            ## configured via the system level nixpkgs options; This saves an
+            ## extra Nixpkgs evaluation, adds consistency, and removes the
+            ## dependency on `NIX_PATH`, which is otherwise used for importing
+            ## Nixpkgs.
             useGlobalPkgs = true;
+
+            ## By default packages will be installed to `$HOME/.nix-profile` but
+            ## they can be installed to `/etc/profiles` if the following is
+            ## added to the system configuration. This option may become the
+            ## default value in the future.
             useUserPackages = true;
-            users.niols = import ./home.nix;
+
+            users = {
+              niols = import ./home.nix;
+            };
+
+            ## The following option gives Home Manager an access to this file's
+            ## inputs through the `specialArgs` option, eg.
+            ## `specialArgs.nix-doom-emacs`.
             extraSpecialArgs = inputs;
           };
         }
