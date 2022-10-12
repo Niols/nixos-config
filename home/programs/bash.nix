9155b7dbce048c4cd3c67b78025b0a7a68b234ed
Niols <niols@niols.fr>
2022-10-12 19:27:40 +0200

    Move `bash` config to own file

---
+++ home/programs/bash.nix
@@ -0,0 +1,31 @@
+{
+  enable = true;
+
+  bashrcExtra = ''
+    ## Keep the prompt when entering `nix shell`.
+    ##
+    ## NOTE: We put this here instead of in
+    ## `home.sessionVariables` because the latter only works for
+    ## login Shells.
+    ##
+    ## cf https://discourse.nixos.org/t/*/8488/23
+    ##
+    NIX_SHELL_PRESERVE_PROMPT=yes
+
+    ## The `nrun` command tries to find the given command name for you,
+    ## either by pulling it from the `PATH` (although you probably already
+    ## tried that) or by pulling it from the `nixpkgs` flake.
+    ##
+    nrun () (
+      cmd=$1; shift
+      if command -v "$cmd" >/dev/null; then
+        "$cmd" "$@"
+      elif nix search nixpkgs "^$cmd\$" >/dev/null 2>&1; then
+        nix run nixpkgs#"$cmd" -- "$@"
+      else
+        echo "Command '$cmd' could not be found in the system or in nixpkgs." >&2
+        exit 127
+      fi
+    )
+  '';
+}
