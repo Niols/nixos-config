d9b214344898db50a36ef4b18ade8897421725f1
Niols <niols@niols.fr>
2022-11-03 23:12:46 +0100

    Use custom OCaml module

--- home/doom.d/init.el
+++ home/doom.d/init.el
@@ -148,7 +148,8 @@
        markdown            ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
        nix                 ; I hereby declare "nix geht mehr!"
-       (ocaml +lsp)        ; an objective camel
+       ;(ocaml +lsp)       ; an objective camel
+       (ocaml-custom +lsp) ; an objective camel [CUSTOM VERSION]
        org                 ; organize your plain life in plain text
        php                 ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
