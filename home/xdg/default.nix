29d614a8b7070347a68bc19b103ef787caaa0de0
Niols <niols@niols.fr>
2022-11-17 22:10:33 +0100

    Fix XDG dirs and add missing ones

--- home/xdg/default.nix
+++ home/xdg/default.nix
@@ -5,9 +5,15 @@
     enable = true;
     desktop = "${config.home.homeDirectory}";
     documents = "${config.home.homeDirectory}/NiolsCloud/Documents";
-    music = "${config.home.homeDirectory}/NiolsCloud/Music";
+    download = "${config.home.homeDirectory}/Downloads";
+    music = "${config.home.homeDirectory}/NiolsCloud/Musique";
     pictures = "${config.home.homeDirectory}/NiolsCloud/Images";
     videos = "${config.home.homeDirectory}/NiolsCloud/Vidéos";
+
+    publicShare = "${config.home.homeDirectory}";
+    templates = "${config.home.homeDirectory}";
+
+    createDirectories = false;
   };
 
   configFile."xfce4/terminal/terminalrc".source =
