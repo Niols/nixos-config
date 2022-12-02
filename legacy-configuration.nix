13a0e3e9d78e9ad98ffc415175acd5f34ce26df2
Niols <niols@niols.fr>
2022-12-02 02:19:18 +0100

    Move X config to own file

--- legacy-configuration.nix
+++ legacy-configuration.nix
@@ -127,56 +127,7 @@
   ############################################################################
   ## X11 windowing system configuration
 
-  services.xserver = {
-    enable = true;
-
-    ## Keymap: US International.
-    ## FIXME: add non-breakable spaces on space bar.
-    ## FIXME: add longer dashes somewhere.
-    ## FIXME: what about three dots?
-    layout = "us";
-    xkbVariant = "intl";
-
-    ## XFCE as desktop manager...
-    desktopManager = {
-      xterm.enable = false;
-      xfce = {
-        enable = true;
-        noDesktop = true;
-        enableXfwm = false;
-      };
-    };
-
-    ## ...with i3 as window manager.
-    windowManager.i3 = {
-      enable = true;
-      extraPackages = with pkgs; [
-        dmenu i3status i3lock
-        python39Packages.py3status ## wrapper around i3status
-      ];
-    };
-
-    ## The display manager choses this combination.
-    displayManager.defaultSession = "xfce+i3";
-
-    ## Enable touchpad support. On the Lenovo X1 Carbon, the touchpad does not
-    ## work so great, so we are trying workarounds as described in:
-    ## https://github.com/NixOS/nixpkgs/issues/19022
-    libinput.enable = true;
-    synaptics.enable = false;
-  };
-
-  ############################################################################
-  ## LightDM
-
-  services.xserver.displayManager.lightdm = {
-    enable = true;
-    background = ./background-image;
-    extraConfig = ''
-      ## Do not hide users, show their `.face`!
-      greeter-hide-users = false
-    '';
-  };
+  services.xserver = import ./xserver.nix { inherit pkgs; };
 
   ############################################################################
   ## GNOME Stuff
@@ -306,11 +257,6 @@
 
   ############################################################################
   ## Background & Transparency
-
-  services.xserver.desktopManager.wallpaper = {
-    mode = "scale";
-    combineScreens = false;
-  };
 
   services.picom.enable = true;
 
---
+++ xserver.nix
@@ -0,0 +1,55 @@
+{ pkgs }:
+
+{
+  enable = true;
+
+  ## Keymap: US International.
+  ## FIXME: add non-breakable spaces on space bar.
+  ## FIXME: add longer dashes somewhere.
+  ## FIXME: what about three dots?
+  layout = "us";
+  xkbVariant = "intl";
+
+  ## XFCE as desktop manager...
+  desktopManager = {
+    xterm.enable = false;
+    xfce = {
+      enable = true;
+      noDesktop = true;
+      enableXfwm = false;
+    };
+    wallpaper = {
+      mode = "scale";
+      combineScreens = false;
+    };
+  };
+
+  ## ...with i3 as window manager.
+  windowManager.i3 = {
+    enable = true;
+    extraPackages = with pkgs; [
+      dmenu i3status i3lock
+      python39Packages.py3status ## wrapper around i3status
+    ];
+  };
+
+  ## The display manager choses this combination.
+  displayManager = {
+    defaultSession = "xfce+i3";
+
+    lightdm = {
+      enable = true;
+      background = ./background-image;
+      extraConfig = ''
+      ## Do not hide users, show their `.face`!
+      greeter-hide-users = false
+    '';
+    };
+  };
+
+  ## Enable touchpad support. On the Lenovo X1 Carbon, the touchpad does not
+  ## work so great, so we are trying workarounds as described in:
+  ## https://github.com/NixOS/nixpkgs/issues/19022
+  libinput.enable = true;
+  synaptics.enable = false;
+}
