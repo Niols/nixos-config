9e9a1f49f2238990820ef6298856b6b7d5e3fae7
Niols <niols@niols.fr>
2022-12-02 12:19:18 +0100

    reintroduce Nix binary caches

--- nix.nix
+++ nix.nix
@@ -18,6 +18,17 @@
       experimental-features = nix-command flakes
     '';
 
+  settings = {
+    substituters = [
+      "https://nix-community.cachix.org"
+      "https://cache.nixos.org/"
+    ];
+
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   ## Garbage-collect automatically everything that is at least a month old. Do
   ## not garbage-collect results of `direnv` or `nix build` as long as there
   ## is a link.
