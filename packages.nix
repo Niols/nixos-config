ffc2e57cb004a15b095f998b6d2c757826984942
Niols <niols@niols.fr>
2023-01-24 20:48:50 +0100

    Add hls wrapper

--- packages.nix
+++ packages.nix
@@ -11,6 +11,19 @@ let ocamlConstraints = {
     };
     ocamlPackages = opam-nix.queryToScope { pkgs = pkgs; } ocamlConstraints;
     ocamlPackages' = map (x: ocamlPackages.${x}) (builtins.attrNames ocamlConstraints);
+
+    ## Emacs for Haskell is not very Nix-aware and expects a binary called
+    ## `haskell-language-server-wrapper`. But Nix environments make this notion
+    ## of wrappers irrelevant and therefore they do not provide the binary in
+    ## question. This compatibility script adds an executable fallback called
+    ## `haskell-language-server-wrapper` that just relays everything to
+    ## `haskell-language-server`. This should make my life easier everywhere.
+    hlsWrapperCompatScript = pkgs.writeShellApplication {
+        name = "haskell-language-server-wrapper";
+        text = ''
+          exec haskell-language-server "$@"
+        '';
+    };
 in
 
 {
@@ -76,6 +89,7 @@ in
     ## H
     htop
     httpie
+    hlsWrapperCompatScript
 
     ## I
     inkscape
