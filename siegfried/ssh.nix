1f55a2d623fa3c6ac093005a7bbc4bcb05373ee1
Niols <niols@niols.fr>
2023-05-11 19:50:52 +0200

    Specify location of host keys

--- siegfried/ssh.nix
+++ siegfried/ssh.nix
@@ -3,6 +3,18 @@ _: {
     openssh = {
       enable = true;
       ports = [ 22 2222 3000 9100 ];
+
+      hostKeys = [
+        {
+          bits = 4096;
+          path = "/etc/ssh/ssh_host_rsa_key";
+          type = "rsa";
+        }
+        {
+          path = "/etc/ssh/ssh_host_ed25519_key";
+          type = "ed25519";
+        }
+      ];
     };
   };
 
