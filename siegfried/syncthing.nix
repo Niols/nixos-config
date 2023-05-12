c48fb1fd624c9e82589cba06d06f23a58801c3b5
Niols <niols@niols.fr>
2023-05-12 13:06:39 +0000

    Move Syncthing behind Nginx

--- siegfried/syncthing.nix
+++ siegfried/syncthing.nix
@@ -4,10 +4,14 @@ _: {
     user = "syncthing";
     dataDir = "/home/syncthing";
     configDir = "/home/syncthing/.config/syncthing";
-    guiAddress = "0.0.0.0:8384"; ## FIXME: hide behind Nginx
+    guiAddress = "127.0.0.1:8384";
     extraOptions.gui = {
       user = "niols";
       password = "syncthingpassword";
     };
+  };
+
+  services.nginx.virtualHosts.syncthing = {
+    locations."/" = { proxyPass = "http://127.0.0.1:8384"; };
   };
 }
