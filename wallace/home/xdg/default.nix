dfed585dd1cf0f5350237924f6b0112de15f0ef8
Niols <niols@niols.fr>
2023-05-15 22:13:26 +0000

    Music and Videos back home

--- wallace/home/xdg/default.nix
+++ wallace/home/xdg/default.nix
@@ -6,9 +6,9 @@
     desktop = "${config.home.homeDirectory}";
     documents = "${config.home.homeDirectory}/NiolsCloud/Documents";
     download = "${config.home.homeDirectory}/Downloads";
-    music = "${config.home.homeDirectory}/NiolsCloud/Musique";
+    music = "${config.home.homeDirectory}/Musique";
     pictures = "${config.home.homeDirectory}/NiolsCloud/Images";
-    videos = "${config.home.homeDirectory}/NiolsCloud/Vidéos";
+    videos = "${config.home.homeDirectory}/Videos";
 
     publicShare = "${config.home.homeDirectory}";
     templates = "${config.home.homeDirectory}";
