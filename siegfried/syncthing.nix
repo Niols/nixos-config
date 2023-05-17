be143d499be5e66aefd6f92e29c5af60cbea3fa1
Niols <niols@niols.fr>
2023-05-17 20:34:00 +0200

    Remove double password for Syncthing
    Nginx already takes care of the basic authentication.
    And it does it better (with Agenix).

--- siegfried/syncthing.nix
+++ siegfried/syncthing.nix
@@ -7,10 +7,6 @@
     dataDir = "/home/syncthing";
     configDir = "/home/syncthing/.config/syncthing";
     guiAddress = "127.0.0.1:8384";
-    extraOptions.gui = {
-      user = "niols";
-      password = "syncthingpassword";
-    };
   };
 
   age.secrets.syncthing-passwd = {
