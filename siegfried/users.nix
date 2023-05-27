034a4d90f9557c7fd45b066413076a8940683e0a
Niols <niols@niols.fr>
2023-05-27 22:14:36 +0000

    Timer to enforce the `public` group in `/srv`

--- siegfried/users.nix
+++ siegfried/users.nix
@@ -9,4 +9,31 @@ _: {
   };
 
   users.groups.public = { };
+
+  ## The following defines a timer that ensures that everything in `/srv` always
+  ## has group `public` and group permissions equal to user permissions. This
+  ## allows other services to freely manipulate data in there.
+
+  ## NOTE: We should probably have a common file for everything that Siegfried
+  ## serves, which would define the NFS but also this timer.
+
+  systemd.timers."make-public" = {
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnBootSec = "5m";
+      OnUnitActiveSec = "5m";
+      Unit = "make-public.service";
+    };
+  };
+
+  systemd.services."make-public" = {
+    serviceConfig = {
+      Type = "oneshot";
+      User = "root";
+    };
+    script = ''
+      chgrp -R public /srv
+      chmod -R g=u /srv
+    '';
+  };
 }
