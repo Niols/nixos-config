51ba25c90ea959071d76c8971e6c96d65f4f8f4b
Niols <niols@niols.fr>
2023-05-27 19:56:33 +0000

    Read-write access to NFS

--- siegfried/nfs.nix
+++ siegfried/nfs.nix
@@ -2,8 +2,8 @@ _: {
   services.nfs.server = {
     enable = true;
     exports = ''
-      /srv       158.178.195.191(r,fsid=0,no_subtree_check)
-      /srv/shows 158.178.195.191(r,nohide,insecure,no_subtree_check)
+      /srv       158.178.195.191(rw,fsid=0,no_subtree_check)
+      /srv/shows 158.178.195.191(rw,nohide,insecure,no_subtree_check)
     '';
     ##           ^^^ Orianne ^^^
   };
