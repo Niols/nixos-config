578dc29d362f1c559f8dfe6fe83efdd9fa572c4e
Niols <niols@niols.fr>
2023-05-27 19:57:06 +0000

    Also serve movies

--- orianne/nfs.nix
+++ orianne/nfs.nix
@@ -6,4 +6,10 @@ _:
     fsType = "nfs";
     options = [ "x-systemd.automount=noauto" "x-systemd.idle-timeout=600" ];
   };
+
+  fileSystems."/srv/movies" = {
+    device = "siegfried.niols.fr:/movies";
+    fsType = "nfs";
+    options = [ "x-systemd.automount=noauto" "x-systemd.idle-timeout=600" ];
+  };
 }
--- siegfried/nfs.nix
+++ siegfried/nfs.nix
@@ -2,8 +2,9 @@ _: {
   services.nfs.server = {
     enable = true;
     exports = ''
-      /srv       158.178.195.191(rw,fsid=0,no_subtree_check)
-      /srv/shows 158.178.195.191(rw,nohide,insecure,no_subtree_check)
+      /srv        158.178.195.191(rw,fsid=0,no_subtree_check)
+      /srv/shows  158.178.195.191(rw,nohide,insecure,no_subtree_check)
+      /srv/movies 158.178.195.191(rw,nohide,insecure,no_subtree_check)
     '';
     ##           ^^^ Orianne ^^^
   };
