8bab59cb882095353de8f9ce89a56827b8793bfb
Niols <niols@niols.fr>
2023-05-27 19:15:46 +0000

    `tvshows` -> `shows`

--- orianne/nfs.nix
+++ orianne/nfs.nix
@@ -1,8 +1,8 @@
 _:
 
 {
-  fileSystems."/srv/tvshows" = {
-    device = "siegfried.niols.fr:/tvshows";
+  fileSystems."/srv/shows" = {
+    device = "siegfried.niols.fr:/shows";
     fsType = "nfs";
     options = [ "x-systemd.automount=noauto" "x-systemd.idle-timeout=600" ];
   };
--- siegfried/nfs.nix
+++ siegfried/nfs.nix
@@ -2,10 +2,10 @@ _: {
   services.nfs.server = {
     enable = true;
     exports = ''
-      /srv         158.178.195.191(rw,fsid=0,no_subtree_check)
-      /srv/tvshows 158.178.195.191(rw,nohide,insecure,no_subtree_check)
+      /srv       158.178.195.191(r,fsid=0,no_subtree_check)
+      /srv/shows 158.178.195.191(r,nohide,insecure,no_subtree_check)
     '';
-    ##             ^^^ Orianne ^^^
+    ##           ^^^ Orianne ^^^
   };
 
   networking.firewall.allowedTCPPorts = [ 2049 ];
