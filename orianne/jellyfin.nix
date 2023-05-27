5f1ecfbc5d92e4e71a1700ff1f8cd7b4c513590d
Niols <niols@niols.fr>
2023-05-27 15:24:36 +0000

    Much simpler Nginx proxy

--- orianne/jellyfin.nix
+++ orianne/jellyfin.nix
@@ -10,56 +10,6 @@ _: {
     forceSSL = true;
     enableACME = true;
 
-    locations."/" = { return = "302 https://$host/web/"; };
-
-    locations."/" = {
-      ## Proxy main Jellyfin traffic
-      proxyPass = "http://127.0.0.1:8096";
-
-      extraConfig = ''
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto $scheme;
-        proxy_set_header X-Forwarded-Protocol $scheme;
-        proxy_set_header X-Forwarded-Host $http_host;
-
-        ## Disable buffering when the nginx proxy gets very resource heavy upon streaming
-        proxy_buffering off;
-      '';
-    };
-
-    ## Location block for /web - This is purely for aesthetics so /web/#!/ works
-    ## instead of having to go to /web/index.html/#!/
-    locations."/web/" = {
-      ## Proxy main Jellyfin traffic
-      proxyPass = "http://127.0.0.1:8096/web/index.html";
-
-      extraConfig = ''
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto $scheme;
-        proxy_set_header X-Forwarded-Protocol $scheme;
-        proxy_set_header X-Forwarded-Host $http_host;
-      '';
-    };
-
-    locations."/socket" = {
-      ## Proxy Jellyfin Websockets traffic
-      proxyPass = "http://127.0.0.1:8096";
-
-      extraConfig = ''
-        proxy_http_version 1.1;
-        proxy_set_header Upgrade $http_upgrade;
-        proxy_set_header Connection "upgrade";
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto $scheme;
-        proxy_set_header X-Forwarded-Protocol $scheme;
-        proxy_set_header X-Forwarded-Host $http_host;
-      '';
-    };
+    locations."/" = { proxyPass = "http://127.0.0.1:8096"; };
   };
 }
