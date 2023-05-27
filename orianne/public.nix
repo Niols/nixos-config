e3e476f7ab2781af95e7746d746f47b0840e8869
Niols <niols@niols.fr>
2023-05-27 22:42:32 +0000

    Also serve a `music/` directory

--- orianne/public.nix
+++ orianne/public.nix
@@ -7,14 +7,20 @@
 _:
 
 {
-  fileSystems."/srv/shows" = {
-    device = "siegfried.niols.fr:/shows";
+  fileSystems."/srv/movies" = {
+    device = "siegfried.niols.fr:/movies";
     fsType = "nfs";
     options = [ "x-systemd.automount=noauto" "x-systemd.idle-timeout=600" ];
   };
 
-  fileSystems."/srv/movies" = {
-    device = "siegfried.niols.fr:/movies";
+  fileSystems."/srv/music" = {
+    device = "siegfried.niols.fr:/music";
+    fsType = "nfs";
+    options = [ "x-systemd.automount=noauto" "x-systemd.idle-timeout=600" ];
+  };
+
+  fileSystems."/srv/shows" = {
+    device = "siegfried.niols.fr:/shows";
     fsType = "nfs";
     options = [ "x-systemd.automount=noauto" "x-systemd.idle-timeout=600" ];
   };
--- siegfried/public.nix
+++ siegfried/public.nix
@@ -40,8 +40,9 @@ _:
     enable = true;
     exports = ''
       /srv        158.178.195.191(rw,fsid=0,no_subtree_check)
-      /srv/shows  158.178.195.191(rw,nohide,insecure,no_subtree_check)
       /srv/movies 158.178.195.191(rw,nohide,insecure,no_subtree_check)
+      /srv/music  158.178.195.191(rw,nohide,insecure,no_subtree_check)
+      /srv/shows  158.178.195.191(rw,nohide,insecure,no_subtree_check)
     '';
     ##            ^^^ Orianne ^^^
   };
