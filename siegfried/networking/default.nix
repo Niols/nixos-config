1e089d2f5b055a496ae461281f597d208e73d6ce
Nicolas Jeannerod <niols@niols.fr>
2023-05-27 19:21:57 +0000

    Enable IPv6 on Siegfried (#33)

--- siegfried/networking/default.nix
+++ siegfried/networking/default.nix
@@ -3,13 +3,20 @@ _: {
 
   networking = {
     defaultGateway = "37.187.6.254";
+    defaultGateway6 = "2001:41d0:a:6ff:ff:ff:ff:ff";
 
     ## Use Google's public DNS servers
-    nameservers = [ "8.8.8.8" "8.8.4.4" ];
+    nameservers =
+      [ "8.8.8.8" "8.8.4.4" "2001:4860:4860::8888" "2001:4860:4860::8844" ];
 
     interfaces.eth0.ipv4.addresses = [{
       address = "37.187.6.180";
       prefixLength = 24;
+    }];
+
+    interfaces.eth0.ipv6.addresses = [{
+      address = "2001:41d0:a:6b4::1";
+      prefixLength = 56;
     }];
 
     ## TODO: Open ports in the firewall.
