800ab426d47f580c345bbd03bbbb048477365dca
Niols <niols@niols.fr>
2023-05-27 15:04:46 +0000

    Proxy traffic to Jellyfin

--- orianne/jellyfin.nix
+++ orianne/jellyfin.nix
@@ -3,4 +3,67 @@ _: {
     enable = true;
     openFirewall = true;
   };
+
+  services.nginx.virtualHosts.media = {
+    serverName = "media.niols.fr";
+
+    forceSSL = true;
+    enableACME = true;
+
+    ## The default `client_max_body_size` is 1M, this might not be enough for
+    ## some posters, etc.
+    clientMaxBodySize = "20M";
+
+    locations."/" = { return = "302 https://$host/web/"; };
+
+    locations."/" = {
+      ## Proxy main Jellyfin traffic
+      proxyPass = "http://127.0.0.1:8096";
+
+      extraConfig = ''
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_set_header X-Forwarded-Protocol $scheme;
+        proxy_set_header X-Forwarded-Host $http_host;
+
+        ## Disable buffering when the nginx proxy gets very resource heavy upon streaming
+        proxy_buffering off;
+      '';
+    };
+
+    ## Location block for /web - This is purely for aesthetics so /web/#!/ works
+    ## instead of having to go to /web/index.html/#!/
+    locations."/web/" = {
+      ## Proxy main Jellyfin traffic
+      proxyPass = "http://127.0.0.1:8096/web/index.html";
+
+      extraConfig = ''
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_set_header X-Forwarded-Protocol $scheme;
+        proxy_set_header X-Forwarded-Host $http_host;
+      '';
+    };
+
+    locations."/socket" = {
+      ## Proxy Jellyfin Websockets traffic
+      proxyPass = "http://127.0.0.1:8096";
+
+      extraConfig = ''
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection "upgrade";
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_set_header X-Forwarded-Protocol $scheme;
+        proxy_set_header X-Forwarded-Host $http_host;
+      '';
+    };
+  };
 }
