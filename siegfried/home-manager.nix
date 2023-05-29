8ffd18cb40f7f037d3594b7fd8e8ddaa278a994a
Niols <niols@niols.fr>
2023-05-29 15:07:29 +0000

    Note to enable Bash

--- siegfried/home-manager.nix
+++ siegfried/home-manager.nix
@@ -3,6 +3,9 @@ _: {
     useUserPackages = true;
     useGlobalPkgs = true;
 
+    ## NOTE: It is important to enable Bash so that Home Manager active
+    ## properly. Otherwise, see Home Manager's documentation.
+
     users.niols = {
       home.stateVersion = "21.05";
       programs.home-manager.enable = true;
