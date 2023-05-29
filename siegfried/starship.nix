098039c76f4c2e62e8ce1de0ed903f963fbf7b90
Niols <niols@niols.fr>
2023-05-29 14:48:04 +0000

    In the right function?

--- siegfried/starship.nix
+++ siegfried/starship.nix
@@ -1,13 +1,19 @@
 _: {
-  imports = [ ../_modules/niols-starship.nix ];
+  home-manager.users.niols = _: {
+    imports = [ ../_modules/niols-starship.nix ];
 
-  home-manager.users.niols.niols-starship = {
-    enable = true;
-    hostcolour = "yellow";
+    niols-starship = {
+      enable = true;
+      hostcolour = "yellow";
+    };
   };
 
-  home-manager.users.root.niols-starship = {
-    enable = true;
-    hostcolour = "red";
+  home-manager.users.root = _: {
+    imports = [ ../_modules/niols-starship.nix ];
+
+    niols-starship = {
+      enable = true;
+      hostcolour = "red";
+    };
   };
 }
