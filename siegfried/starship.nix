075ad9ea0554697ae478ac105239efa926f0beb5
Niols <niols@niols.fr>
2023-05-29 14:41:13 +0000

    Enable Starship on Siegfried

---
+++ siegfried/starship.nix
@@ -0,0 +1,11 @@
+_: {
+  home-manager.users.niols.niols-starship = {
+    enable = true;
+    hostcolour = "yellow";
+  };
+
+  home-manager.users.root.niols-starship = {
+    enable = true;
+    hostcolour = "red";
+  };
+}
