b887a3ccabcb8a4c0ccb513e0a81fd6bccd868c6
Niols <niols@niols.fr>
2023-06-26 17:10:32 +0200

    Fixed password for Niols on Siegfried

--- siegfried/users.nix
+++ siegfried/users.nix
@@ -1,10 +1,16 @@
-_: {
+{ config, secrets, ... }:
+
+{
   users.users.niols = {
     isNormalUser = true;
     extraGroups = [ "public" "wheel" ];
+
+    passwordFile = config.age.secrets.siegfried-niols-password.path;
 
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEElREJN0AC7lbp+5X204pQ5r030IbgCllsIxyU3iiKY niols@wallace"
     ];
   };
+
+  age.secrets.siegfried-niols-password.file = "${secrets}/siegfried-niols-password.age";
 }
