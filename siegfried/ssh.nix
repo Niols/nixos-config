8666b0f120353b3275dd468e102576e6616cd3a6
Niols <niols@niols.fr>
2023-07-06 15:27:49 +0000

    Cleanup SSH file; remove explicit ports

--- siegfried/ssh.nix
+++ siegfried/ssh.nix
@@ -1,21 +1,18 @@
 _: {
-  services = {
-    openssh = {
-      enable = true;
-      ports = [ 22 2222 3000 9100 ];
+  services.openssh = {
+    enable = true;
 
-      hostKeys = [
-        {
-          bits = 4096;
-          path = "/etc/ssh/ssh_host_rsa_key";
-          type = "rsa";
-        }
-        {
-          path = "/etc/ssh/ssh_host_ed25519_key";
-          type = "ed25519";
-        }
-      ];
-    };
+    hostKeys = [
+      {
+        bits = 4096;
+        path = "/etc/ssh/ssh_host_rsa_key";
+        type = "rsa";
+      }
+      {
+        path = "/etc/ssh/ssh_host_ed25519_key";
+        type = "ed25519";
+      }
+    ];
   };
 
   programs.mosh.enable = true;
