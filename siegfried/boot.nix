d93e408ca896e3f9d408dabd625df13561a4a476
Niols <niols@niols.fr>
2023-08-27 22:27:32 +0000

    Basic config for new Siegfried

---
+++ siegfried/boot.nix
@@ -0,0 +1,14 @@
+_:
+
+{
+  boot = {
+    ## Necessary for predictable names, eg. `eth0` for ethernet.
+    ## REVIEW: what about networking.usePredictableInterfaceNames = false?
+    kernelParams = [ "net.ifnames=0" ];
+
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+  };
+}
---
+++ siegfried/motd.nix
@@ -0,0 +1,10 @@
+_: {
+  imports = [ ../_modules/niols-motd.nix ];
+
+  niols-motd = {
+    enable = true;
+    hostname = "Siegfried";
+    hostcolour = "yellow";
+    noSwap = true;
+  };
+}
