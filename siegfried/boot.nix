677e2d1a3a4fd3342bd878781463aabfd526f01a
Niols <niols@niols.fr>
2023-08-30 23:07:45 +0000

    Do not specify bootloader

--- siegfried/boot.nix
+++ siegfried/boot.nix
@@ -5,10 +5,5 @@ _:
     ## Necessary for predictable names, eg. `eth0` for ethernet.
     ## REVIEW: what about networking.usePredictableInterfaceNames = false?
     kernelParams = [ "net.ifnames=0" ];
-
-    loader = {
-      systemd-boot.enable = true;
-      efi.canTouchEfiVariables = true;
-    };
   };
 }
