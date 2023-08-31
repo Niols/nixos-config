78ffb8e4af39470b6ed785cb0022de721cb16598
Niols <niols@niols.fr>
2023-08-31 12:49:21 +0000

    Set password for Niols

--- siegfried/users.nix
+++ siegfried/users.nix
@@ -1,4 +1,9 @@
-_: {
+{ config, secrets, ... }:
+
+{
+  ## Make each system activation forcefully replace the current status of users.
+  users.mutableUsers = false;
+
   users.users.niols = {
     isNormalUser = true;
     extraGroups = [ "wheel" ];
@@ -6,5 +11,10 @@ _: {
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEElREJN0AC7lbp+5X204pQ5r030IbgCllsIxyU3iiKY niols@wallace"
     ];
+    passwordFile = config.age.secrets.password-niols.path;
+  };
+
+  age.secrets.password-niols = {
+    file = "${secrets}/password-siegfried-niols.age";
   };
 }
