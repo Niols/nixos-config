084bcd9f44dc9b33c2044795b4e810b957168b02
Niols <niols@niols.fr>
2023-09-01 12:23:34 +0000

    Give access to Hester to Niols

--- siegfried/storage.nix
+++ siegfried/storage.nix
@@ -11,8 +11,13 @@
       "x-systemd.device-timeout=5s"
       "x-systemd.mount-timeout=5s"
       "credentials=${config.age.secrets.hester-samba-credentials.path}"
+      "gid=hester"
+      "dir_mode=0775"
+      "file_mode=0664"
     ];
   };
+
+  users.groups.hester.members = [ "niols" ];
 
   age.secrets.hester-samba-credentials = {
     file = "${secrets}/hester-samba-credentials.age";
