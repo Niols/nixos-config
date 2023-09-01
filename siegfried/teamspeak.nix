256fb348e878ef6a6e17f93e8ce4564742b1a321
Niols <niols@niols.fr>
2023-09-01 12:31:55 +0000

    Add teamspeak to Hester group

--- siegfried/teamspeak.nix
+++ siegfried/teamspeak.nix
@@ -5,7 +5,10 @@
     enable = true;
     openFirewall = true;
     dataDir = "/hester/services/teamspeak";
+    logDir = "/hester/services/teamspeak/logs";
   };
+
+  users.groups.hester.members = [ "teamspeak" ];
 
   nixpkgs.config.allowUnfreePredicate =
     (pkg: builtins.elem (pkgs.lib.getName pkg) [ "teamspeak-server" ]);
