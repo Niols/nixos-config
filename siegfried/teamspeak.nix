a53b975f08ce3d9cd3fd649b5f5ae436762ab87e
Niols <niols@niols.fr>
2023-09-01 12:33:11 +0000

    Fix typo

--- siegfried/teamspeak.nix
+++ siegfried/teamspeak.nix
@@ -5,7 +5,7 @@
     enable = true;
     openFirewall = true;
     dataDir = "/hester/services/teamspeak";
-    logDir = "/hester/services/teamspeak/logs";
+    logPath = "/hester/services/teamspeak/logs";
   };
 
   users.groups.hester.members = [ "teamspeak" ];
