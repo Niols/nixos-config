d5094d1b55c495958de0ad6aec3bbf0e6be14d40
Niols <niols@niols.fr>
2023-09-20 16:21:30 +0000

    Add Syncthing to hester group

--- siegfried/syncthing.nix
+++ siegfried/syncthing.nix
@@ -3,8 +3,7 @@
 {
   services.syncthing = {
     enable = true;
-    # user = "syncthing";
-    # group = "public";
+    user = "syncthing";
     dataDir = "/hester/services/syncthing";
     configDir = "/hester/services/syncthing/config";
     guiAddress = "127.0.0.1:8384";
@@ -16,7 +15,7 @@
     };
   };
 
-  # users.groups.hester.members = [ "syncthing" ];
+  users.groups.hester.members = [ "syncthing" ];
 
   systemd.services.syncthing-init.unitConfig.RequiresMountsFor = "/hester";
   systemd.services.syncthing.unitConfig.RequiresMountsFor = "/hester";
