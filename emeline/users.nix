700deab56e992a57aae382d0fa7afe273f35b360
Niols <niols@niols.fr>
2023-09-22 22:59:48 +0000

    `passwordFile` -> `hashedPasswordFile`

--- emeline/users.nix
+++ emeline/users.nix
@@ -11,7 +11,7 @@
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEElREJN0AC7lbp+5X204pQ5r030IbgCllsIxyU3iiKY niols@wallace"
     ];
-    passwordFile = config.age.secrets.password-niols.path;
+    hashedPasswordFile = config.age.secrets.password-niols.path;
   };
 
   age.secrets.password-niols = {
