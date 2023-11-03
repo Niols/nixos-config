ce05ef9438f7c76ce132bd89eb20f02e363e75c6
Niols <niols@niols.fr>
2023-11-03 12:38:16 +0100

    Also use personal key in `/etc/nixos`

--- wallace/home/programs/git.nix
+++ wallace/home/programs/git.nix
@@ -29,6 +29,7 @@
         "gitdir:~/git/colis/**"
         "gitdir:~/git/rscds/**"
         "gitdir:~/.opam/plugins/opam-publish/repos/**"
+        "gitdir:/etc/nixos"
       ];
       contents.user = {
         name = "Niols";
