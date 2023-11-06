a8423e49513f62a92e4a5e0108bf4036f6b3f5ef
Niols <niols@niols.fr>
2023-11-06 16:25:31 +0000

    New Orianne

--- orianne/boot.nix
+++ orianne/boot.nix
@@ -5,5 +5,10 @@ _:
     ## Necessary for predictable names, eg. `eth0` for ethernet.
     ## REVIEW: what about networking.usePredictableInterfaceNames = false?
     kernelParams = [ "net.ifnames=0" ];
+
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
   };
 }
--- orianne/hardware-configuration.nix
+++ orianne/hardware-configuration.nix
@@ -1,31 +1,31 @@
-{ modulesPath, ... }:
+{ lib, modulesPath, ... }:
 
 {
   imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
 
   fileSystems = {
     "/" = {
-      device = "/dev/disk/by-uuid/bc49ebc1-f241-43da-9b4e-1f4c91405121";
+      device = "/dev/disk/by-uuid/4a1a8170-c1cf-4ef2-b27a-ced57fa60ad7";
       fsType = "ext4";
     };
 
     "/boot" = {
-      device = "/dev/disk/by-uuid/329D-162C";
+      device = "/dev/disk/by-uuid/9468-FE29";
       fsType = "vfat";
     };
   };
 
   boot = {
-    loader = {
-      systemd-boot.enable = true;
-      efi.canTouchEfiVariables = true;
-    };
-
     initrd = {
-      availableKernelModules = [ "nvme" ];
+      availableKernelModules =
+        [ "xhci_pci" "virtio_pci" "virtio_scsi" "usbhid" ];
       kernelModules = [ ];
     };
     kernelModules = [ ];
     extraModulePackages = [ ];
   };
+
+  ## REVIEW: not sure if we need those
+  networking.useDHCP = lib.mkDefault true;
+  nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
 }
