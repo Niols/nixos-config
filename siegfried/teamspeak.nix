bf591bf28b1225b4ce5e92eb89c63c367916f8c8
Niols <niols@niols.fr>
2023-11-19 11:12:53 +0000

    Setup borgbackup for TeamSpeak

--- siegfried/teamspeak.nix
+++ siegfried/teamspeak.nix
@@ -10,4 +10,13 @@
 
   nixpkgs.config.allowUnfreePredicate =
     (pkg: builtins.elem (pkgs.lib.getName pkg) [ "teamspeak-server" ]);
+
+  services.borgbackup.jobs.teamspeak = {
+    paths = "/var/lib/teamspeak";
+    encryption.mode = "none";
+    repo = "/hester/backups/teamspeak";
+    startAt = "*-*-* 06:00:00";
+  };
+  systemd.services.borgbackup-job-teamspeak.unitConfig.RequiresMountsFor =
+    "/hester";
 }
