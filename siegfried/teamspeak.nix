cc15a8caab7f077fb1782008870e42dae72d9fca
Niols <niols@niols.fr>
2023-11-19 10:44:00 +0000

    Move teamspeak to `/var`
    It does not work on `/hester` because TeamSpeak uses sqlite's WAL mode
    which does not do well with network drives.

--- siegfried/teamspeak.nix
+++ siegfried/teamspeak.nix
@@ -4,14 +4,10 @@
   services.teamspeak3 = {
     enable = true;
     openFirewall = true;
-    dataDir = "/hester/services/teamspeak";
-    logPath = "/hester/services/teamspeak/logs";
+    dataDir = "/var/lib/teamspeak";
+    logPath = "/var/log/teamspeak";
   };
-
-  users.groups.hester.members = [ "teamspeak" ];
 
   nixpkgs.config.allowUnfreePredicate =
     (pkg: builtins.elem (pkgs.lib.getName pkg) [ "teamspeak-server" ]);
-
-  systemd.services.teamspeak3-server.unitConfig.RequiresMountsFor = "/hester";
 }
