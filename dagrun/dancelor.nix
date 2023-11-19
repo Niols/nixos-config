19dbad6b85ce7e3ad6632068355aa15118671686
Niols <niols@niols.fr>
2023-11-19 13:34:02 +0000

    Getting there

--- dagrun/dancelor.nix
+++ dagrun/dancelor.nix
@@ -1,6 +1,20 @@
 { config, secrets, dancelor, pkgs, ... }:
 
-let dancelor' = dancelor.packages.x86_64-linux.dancelor;
+let
+  dancelor' = dancelor.packages.x86_64-linux.dancelor;
+  run-dancelor-server = pkgs.writeShellApplication {
+    name = "run-dancelor-server";
+    text = ''
+      mkdir -p /var/cache/dancelor/{version,set,book}
+
+      ${dancelor'}/bin/dancelor-server \
+        --cache /var/cache/dancelor \
+        --database /var/lib/dancelor/database \
+        --loglevel info \
+        --port 6872 \
+        --share ${dancelor'}/share
+    '';
+  };
 
 in {
   users.users.dancelor = {
@@ -11,19 +25,7 @@ in {
 
   systemd.services.dancelor = {
     serviceConfig = {
-      ExecStart = pkgs.writeShellApplication {
-        name = "run-dancelor-server";
-        text = ''
-          mkdir -p /var/cache/dancelor/{version,set,book}
-
-          ${dancelor'}/bin/dancelor-server \
-            --cache /var/cache/dancelor \
-            --database /var/lib/dancelor/database \
-            --loglevel info \
-            --port 6872 \
-            --share ${dancelor'}/share
-        '';
-      };
+      ExecStart = "${run-dancelor-server}/bin/run-dancelor-server";
       Restart = "always";
       User = "dancelor";
       Group = "dancelor";
