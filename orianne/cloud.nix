7d9114ceaa8c1735eddbaad75a9cba8b08aba191
Niols <niols@niols.fr>
2023-12-28 18:35:48 +0100

    ensureDBOwnership

--- orianne/cloud.nix
+++ orianne/cloud.nix
@@ -46,11 +46,10 @@
   services.postgresql = {
     enable = true;
 
-    # Ensure the database, user, and permissions always exist
     ensureDatabases = [ "nextcloud" ];
     ensureUsers = [{
       name = "nextcloud";
-      ensurePermissions."DATABASE nextcloud" = "ALL PRIVILEGES";
+      ensureDBOwnership = true;
     }];
   };
 
