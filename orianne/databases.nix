fb04da4a563b72ff90e48906ce29c7697308e600
Niols <niols@niols.fr>
2024-01-08 19:01:21 +0000

    Revert "Test backups"
    This reverts commit ed79a1501e75d64f9f270b58b9730d7bbd8c7721.

--- orianne/databases.nix
+++ orianne/databases.nix
@@ -3,7 +3,7 @@
 
   services.postgresqlBackup = {
     enable = true;
-    startAt = "*-*-* 19:00:00";
+    startAt = "*-*-* 04:15:00";
     location = "/hester/backups/databases/orianne";
   };
   systemd.services.postgresqlBackup.unitConfig.RequiresMountsFor =
