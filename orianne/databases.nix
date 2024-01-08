ed79a1501e75d64f9f270b58b9730d7bbd8c7721
Niols <niols@niols.fr>
2024-01-08 18:52:33 +0000

    Test backups

--- orianne/databases.nix
+++ orianne/databases.nix
@@ -3,7 +3,7 @@
 
   services.postgresqlBackup = {
     enable = true;
-    startAt = "*-*-* 04:15:00";
+    startAt = "*-*-* 19:00:00";
     location = "/hester/backups/databases/orianne";
   };
   systemd.services.postgresqlBackup.unitConfig.RequiresMountsFor = "/hester";
