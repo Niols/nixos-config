289897dafec7118b70fd02f9840db87eb9671884
Niols <niols@niols.fr>
2024-01-10 18:32:49 +0000

    Database and Nginx

--- dagrun/matrix.nix
+++ dagrun/matrix.nix
@@ -1,1 +1,30 @@
-{ }
+{
+  services.postgresql = {
+    ensureDatabases = [ "matrix-synapse" ];
+    ensureUsers = [{
+      name = "matrix-synapse";
+      ensureDBOwnership = true;
+    }];
+    ## All databases are backed up daily. See `databases.nix`.
+  };
+
+  services.nginx.virtualHosts.matrix = {
+    serverName = "matrix.niols.fr";
+
+    enableACME = true;
+    forceSSL = true;
+
+    locations = {
+      ## It's also possible to do a redirect here or something else as this
+      ## virtual host is not needed for Matrix. It's recommended though to
+      ## *not put* element here. See the NixOS manual.
+      "/".extraConfig = "return 404;";
+
+      ## Forward all Matrix API calls to the synapse Matrix homeserver. A
+      ## trailing slash *must not* be used here. Forward requests for e.g. SSO
+      ## and password-resets.
+      "/_matrix".proxyPass = "http://[::1]:8008";
+      "/_synapse/client".proxyPass = "http://[::1]:8008";
+    };
+  };
+}
