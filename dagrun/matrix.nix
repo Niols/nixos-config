934e1d1b0e7a34e1f35b8f3a2a2f46d9cc345c54
Niols <niols@niols.fr>
2024-01-11 11:08:56 +0000

    Enter synapse

--- dagrun/matrix.nix
+++ dagrun/matrix.nix
@@ -1,3 +1,5 @@
+{ config, secrets, ... }:
+
 {
   services.postgresql = {
     ensureDatabases = [ "matrix-synapse" ];
@@ -28,4 +30,31 @@
       "/_synapse/client".proxyPass = "http://[::1]:8008";
     };
   };
+
+  services.matrix-synapse = {
+    enable = true;
+    settings = {
+      server_name = "niols.fr";
+      public_baseurl = "https://matrix.niols.fr";
+      listeners = [{
+        port = 8008;
+        bind_addresses = [ "::1" "127.0.0.1" ];
+        type = "http";
+        tls = false;
+        x_forwarded = true;
+        resources = [{
+          names = [ "client" "federation" ];
+          compress = false;
+        }];
+      }];
+      signing_key_path = config.age.secrets.matrix-synapse-signing-key.path;
+      extraConfigFiles =
+        [ config.age.secrets.matrix-synapse-registration-secret.path ];
+    };
+  };
+
+  age.secrets.matrix-synapse-registration-secret.file =
+    "${secrets}/matrix-synapse-registration-secret.age";
+  age.secrets.matrix-synapse-signing-key.file =
+    "${secrets}/matrix-synapse-signing-key.age";
 }
