cec6f9eb4e4c44c9f025f4c97edd57fdb321de2b
Niols <niols@niols.fr>
2024-02-09 12:28:36 +0100

    Add well-known for Matrix

--- siegfried/web.nix
+++ siegfried/web.nix
@@ -14,6 +14,34 @@
       index = "index.html";
       tryFiles = "$uri $uri/ =404";
     };
+
+    ## Well-known entries for Matrix. They are necessary because the domain is
+    ## `niols.fr` (eg. in `@niols:niols.fr`) but the server name is
+    ## `matrix.niols.fr`. It might even be on a different machine. `server` is
+    ## for federation and `client` is for Matrix clients.
+    ## FIXME: Move this closer to Matrix, but this is on another machine.
+    locations."= /.well-known/matrix/server".extraConfig = ''
+      default_type application/json;
+      add_header Access-Control-Allow-Origin *;
+      return 200 '{"m.server": "matrix.niols.fr:443"}';
+      ## Repeat headers from the server context.
+      add_header Strict-Transport-Security $hsts_header;
+      add_header Referrer-Policy origin-when-cross-origin;
+      add_header X-Frame-Options DENY;
+      add_header X-Content-Type-Options nosniff;
+      add_header X-XSS-Protection "1; mode=block";
+    '';
+    locations."= /.well-known/matrix/client".extraConfig = ''
+      default_type application/json;
+      add_header Access-Control-Allow-Origin *;
+      return 200 '{"m.homeserver": {"base_url": "https://matrix.niols.fr/"}}';
+      ## Repeat headers from the server context.
+      add_header Strict-Transport-Security $hsts_header;
+      add_header Referrer-Policy origin-when-cross-origin;
+      add_header X-Frame-Options DENY;
+      add_header X-Content-Type-Options nosniff;
+      add_header X-XSS-Protection "1; mode=block";
+    '';
   };
 
   systemd.services.nginx.unitConfig = {
