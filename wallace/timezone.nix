7bd0da4dc2b5c5c39d791e9ace8e0858b6c540f1
Niols <niols@niols.fr>
2024-02-12 11:14:31 +0000

    Fix automated detection of timezone

--- wallace/timezone.nix
+++ wallace/timezone.nix
@@ -1,9 +1,13 @@
-{ lib, ... }: {
+{
   ############################################################################
   ## Time zone and internationalisation
 
-  time.timeZone = lib.mkDefault "Europe/Paris";
-  services.automatic-timezoned.enable = true;
+  ## I have tried `automatic-timezoned` that does not get the right timezone
+  ## when I am in Iceland and `tzupdate` that gets it right but does not update
+  ## the actual timezone (visible in `timedatectl`). This is the combination
+  ## that works for me in Iceland:
+  services.geoclue2.enable = true;
+  services.localtimed.enable = true;
 
   i18n.defaultLocale = "en_GB.UTF-8";
   console = {
