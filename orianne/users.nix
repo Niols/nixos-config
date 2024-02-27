5bcb9ceddcb49cccf88242521f8421bf9807d41d
Niols <niols@niols.fr>
2024-02-27 12:05:30 +0000

    Public keys for Philippe

--- orianne/users.nix
+++ orianne/users.nix
@@ -10,6 +10,7 @@
 
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEElREJN0AC7lbp+5X204pQ5r030IbgCllsIxyU3iiKY niols@wallace"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO3cvIpLy9PzLTNV2OaPI4AynVMhYmxa4WiwsARA+wtK niols@philippe"
     ];
     hashedPasswordFile = config.age.secrets.password-niols.path;
   };
--- siegfried/users.nix
+++ siegfried/users.nix
@@ -10,6 +10,7 @@
 
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEElREJN0AC7lbp+5X204pQ5r030IbgCllsIxyU3iiKY niols@wallace"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMo6bWfvWqI5+eNlFr51d1cpBz3ms8dwOySf1WQzbJ84 niols@philippe"
     ];
     hashedPasswordFile = config.age.secrets.password-niols.path;
   };
