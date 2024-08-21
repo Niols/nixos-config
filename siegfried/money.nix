ae1d84eca68976ba983ed53b5c9118a9b802f1c1
Niols <niols@niols.fr>
2024-08-21 10:13:27 +0200

    Add Firefly III Data Importer

--- siegfried/money.nix
+++ siegfried/money.nix
@@ -1,8 +1,92 @@
-{ config, secrets, ... }:
+{ config, secrets, pkgs, lib, ... }:
 
 let
+  inherit (lib.strings) concatLines toShellVars removeSuffix hasSuffix;
+  inherit (lib.attrsets)
+    mapAttrsToList genAttrs filterAttrs mapAttrs' nameValuePair;
+
   url = "money.niols.fr";
   dataDir = "/var/lib/firefly-iii";
+
+  ## Data Importer. FIXME: This violates the way flakes work. This should be
+  ## moved to nixpkgs, maybe even contributed to the Firefly III package?
+  cfg = config.services.firefly-iii;
+  diDataDir = "${cfg.dataDir}/data-importer";
+  diUrlPrefix = "/import";
+  diSettings = { };
+  diVer = "1.5.2";
+  diSrc = pkgs.stdenv.mkDerivation rec {
+    name = "firefly-iii-data-importer-${diVer}";
+    src = pkgs.fetchzip {
+      url =
+        "https://github.com/firefly-iii/data-importer/releases/download/v${diVer}/DataImporter-v${diVer}.tar.gz";
+      hash = "sha256-Z+BQEFbZhxSQIySMJq9FKbuAIQ9LOx8dG3MgkhOV0/w=";
+      stripRoot = false;
+    };
+    phases = [ "installPhase" ];
+    installPhase = ''
+      mkdir $out
+      ${pkgs.rsync}/bin/rsync -a ${src}/ $out --exclude=storage
+      chmod +w $out
+      ln -s ${diDataDir}/storage $out/storage
+      cat <<EOF > $out/.env
+      FIREFLY_III_URL=https://money.niols.fr
+      TRUSTED_PROXIES="*"
+      LOG_LEVEL=debug
+      ASSET_URL=${diUrlPrefix}
+      EOF
+    '';
+  };
+
+  di-env-file-values =
+    mapAttrs' (n: v: nameValuePair (removeSuffix "_FILE" n) v)
+    (filterAttrs (n: _v: hasSuffix "_FILE" n) diSettings);
+  di-env-nonfile-values = filterAttrs (n: _v: !hasSuffix "_FILE" n) diSettings;
+
+  firefly-iii-data-importer-maintenance =
+    pkgs.writeShellScript "firefly-iii-data-importer-maintenance.sh" ''
+      set -a
+      ${toShellVars di-env-nonfile-values}
+      ${concatLines
+      (mapAttrsToList (n: v: "${n}=$(< ${v})") di-env-file-values)}
+      set +a
+    '';
+
+  ## FIXME: Almost copied as-is from Firefly III
+  commonServiceConfig = {
+    Type = "oneshot";
+    User = cfg.user;
+    Group = cfg.group;
+    StateDirectory = "firefly-iii-data-importer";
+    ReadWritePaths = [ diDataDir ];
+    WorkingDirectory = diSrc;
+    PrivateTmp = true;
+    PrivateDevices = true;
+    CapabilityBoundingSet = "";
+    AmbientCapabilities = "";
+    ProtectSystem = "strict";
+    ProtectKernelTunables = true;
+    ProtectKernelModules = true;
+    ProtectControlGroups = true;
+    ProtectClock = true;
+    ProtectHostname = true;
+    ProtectHome = "tmpfs";
+    ProtectKernelLogs = true;
+    ProtectProc = "invisible";
+    ProcSubset = "pid";
+    PrivateNetwork = false;
+    RestrictAddressFamilies = "AF_INET AF_INET6 AF_UNIX";
+    SystemCallArchitectures = "native";
+    SystemCallFilter =
+      [ "@system-service @resources" "~@obsolete @privileged" ];
+    RestrictSUIDSGID = true;
+    RemoveIPC = true;
+    NoNewPrivileges = true;
+    RestrictRealtime = true;
+    RestrictNamespaces = true;
+    LockPersonality = true;
+    PrivateUsers = true;
+  };
 
 in {
   services.firefly-iii = {
@@ -19,10 +103,11 @@ in {
     };
   };
 
-  services.nginx.virtualHosts.${url} = {
-    forceSSL = true;
-    enableACME = true;
-  };
+  ## FIXME: Re-enable when merging cleanly with data importer
+  # services.nginx.virtualHosts.${url} = {
+  #   forceSSL = true;
+  #   enableACME = true;
+  # };
 
   age.secrets.firefly-iii-app-key-file = {
     file = "${secrets}/firefly-iii-app-key-file.age";
@@ -51,4 +136,95 @@ in {
     "${secrets}/hester-firefly-iii-backup-identity.age";
   age.secrets.hester-firefly-iii-backup-repokey.file =
     "${secrets}/hester-firefly-iii-backup-repokey.age";
+
+  ############################################################################
+  ############################################################################
+  ############################################################################
+
+  services.phpfpm.pools.firefly-iii-data-importer = {
+    inherit (cfg) user group;
+    phpPackage = cfg.package.phpPackage;
+    phpOptions = ''
+      log_errors = on
+    '';
+    settings = {
+      "listen.mode" = "0660";
+      "listen.owner" = cfg.user;
+      "listen.group" = cfg.group;
+      "clear_env" = "no";
+    } // cfg.poolConfig;
+  };
+
+  systemd.services.firefly-iii-data-importer-setup = {
+    after = [ "postgresql.service" "mysql.service" ];
+    requiredBy = [ "phpfpm-firefly-iii-data-importer.service" ];
+    before = [ "phpfpm-firefly-iii-data-importer.service" ];
+    serviceConfig = {
+      ExecStart = firefly-iii-data-importer-maintenance;
+      RemainAfterExit = true;
+    } // commonServiceConfig;
+    unitConfig.JoinsNamespaceOf = "phpfpm-firefly-iii-data-importer.service";
+    restartTriggers = [ cfg.package ];
+  };
+
+  services.nginx.virtualHosts.${url} = {
+    forceSSL = true;
+    enableACME = true;
+
+    locations = {
+      "= ${diUrlPrefix}".extraConfig = ''
+        return 301 ${diUrlPrefix}/;
+      '';
+
+      "${diUrlPrefix}/" = {
+        alias = "${diSrc}/public/";
+        ## NOTE: The double `diUrlPrefix` and the middle `/` are not mistakes
+        ## but a mitigation of https://trac.nginx.org/nginx/ticket/97
+        tryFiles =
+          "$uri $uri/ ${diUrlPrefix}/${diUrlPrefix}/index.php?$query_string";
+        index = "index.php";
+        extraConfig = ''
+          sendfile off;
+
+          location ~ \.php$ {
+            include ${config.services.nginx.package}/conf/fastcgi_params;
+            fastcgi_param SCRIPT_FILENAME $request_filename;
+            fastcgi_param modHeadersAvailable true; #Avoid sending the security headers twice
+            fastcgi_pass unix:${config.services.phpfpm.pools.firefly-iii-data-importer.socket};
+          }
+        '';
+      };
+    };
+  };
+
+  systemd.tmpfiles.settings."10-firefly-iii-data-importer" = genAttrs [
+    "${diDataDir}/storage"
+    "${diDataDir}/storage/app"
+    "${diDataDir}/storage/app/public"
+    "${diDataDir}/storage/configurations"
+    "${diDataDir}/storage/conversion-routines"
+    "${diDataDir}/storage/debugbar"
+    "${diDataDir}/storage/framework"
+    "${diDataDir}/storage/framework/cache"
+    "${diDataDir}/storage/framework/cache/data"
+    "${diDataDir}/storage/framework/sessions"
+    "${diDataDir}/storage/framework/testing"
+    "${diDataDir}/storage/framework/views"
+    "${diDataDir}/storage/jobs"
+    "${diDataDir}/storage/logs"
+    "${diDataDir}/storage/submission-routines"
+    "${diDataDir}/storage/uploads"
+  ] (_n: {
+    d = {
+      group = cfg.group;
+      mode = "0700";
+      user = cfg.user;
+    };
+  }) // {
+    "${diDataDir}".d = {
+      group = cfg.group;
+      mode = "0710";
+      user = cfg.user;
+    };
+  };
 }
