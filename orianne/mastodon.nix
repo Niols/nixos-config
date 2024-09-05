76d18df638914e2712497de72fb9aeaa241ca51d
Niols <niols@niols.fr>
2024-09-05 20:24:56 +0200

    Enable Mastodon on Orianne

---
+++ orianne/mastodon.nix
@@ -0,0 +1,30 @@
+{ config, secrets, ... }:
+
+{
+  services.mastodon = {
+    enable = true;
+
+    localDomain = "mastodon.niols.fr";
+    configureNginx = true;
+
+    ## Number of processes used by the mastodon-streaming service. Recommended
+    ## is the amount of CPU cores minus one.
+    streamingProcesses = 3;
+
+    extraConfig.SINGLE_USER_MODE = "true";
+
+    smtp = {
+      fromAddress = "no-reply@niols.fr";
+
+      host = "mail.infomaniak.com";
+      port = 465;
+
+      authenticate = true;
+      user = "no-reply@niols.fr";
+      passwordFile = config.age.secrets.mastodon-noreply-password.path;
+    };
+  };
+
+  age.secrets.mastodon-noreply-password.file =
+    "${secrets}/mastodon-noreply-password.age";
+}
