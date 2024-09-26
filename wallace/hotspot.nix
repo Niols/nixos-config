0ade8a9a14625f50b5a4ed7b2f3973f4c0ad9885
Niols <niols@niols.fr>
2024-09-26 09:34:06 +0200

    Prevent access point from starting automatically

--- wallace/hotspot.nix
+++ wallace/hotspot.nix
@@ -1,12 +1,20 @@
-{
-  #   services.create_ap = {
-  #     enable = true;
-  #     settings = {
-  #       INTERNET_IFACE = "wlp0s20f3";
-  #       WIFI_IFACE = "wlp0s20f3";
-  #       SSID = "Wallace";
-  #       PASSPHRASE = "ReKuYm05";
-  #       FREQ_BAND = "2.4";
-  #     };
-  #   };
+{ lib, ... }:
+
+let inherit (lib) mkForce;
+
+in {
+  services.create_ap = {
+    enable = true;
+    settings = {
+      INTERNET_IFACE = "wlp0s20f3";
+      WIFI_IFACE = "wlp0s20f3";
+      SSID = "Wallace";
+      PASSPHRASE = "ReKuYm05";
+      FREQ_BAND = "2.4";
+    };
+  };
+
+  ## Do not make the unit wanted by anything, such that it will exist but not
+  ## start automatically on start-up.
+  systemd.services.create_ap.wantedBy = mkForce [ ];
 }
