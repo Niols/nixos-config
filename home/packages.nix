70cfac2e59663e88ec95894757daa5c875a39c4d
Niols <niols@niols.fr>
2024-10-01 11:08:06 +0200

    Note on handling packages

--- home/packages.nix
+++ home/packages.nix
@@ -1,5 +1,10 @@
 { pkgs, ... }:
 
+## TODO: Split packages in categories, such as “admin utils” (fd, rg, htop), IDE
+## (Emacs, with all its packages) and “fancy desktop stuff” (GIMP, Inkscape),
+## etc. Use that to easily install on different machines, and share between Home
+## Manager and NixOS configurations.
+
 {
   home.packages = with pkgs; [
     ## Necessary for i3; I much prefer running it standalone.
