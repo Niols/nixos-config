8b760a35c61738cca9925992a430343c94b86a9c
Niols <niols@niols.fr>
2024-10-14 14:39:31 +0200

    Wireguard config for Fediversity

--- wallace/network.nix
+++ wallace/network.nix
@@ -1,4 +1,4 @@
-{ lib, ... }:
+{ lib, secrets, config, ... }:
 
 let inherit (lib) mkForce;
 
@@ -19,4 +19,28 @@ in {
   ## Do not make the unit wanted by anything, such that it will exist but not
   ## start automatically on start-up.
   systemd.services.create_ap.wantedBy = mkForce [ ];
+
+  ##############################################################################
+  ## VPN for the Fediversity project
+
+  networking.wireguard = {
+    enable = true;
+    interfaces = {
+      fediversity = {
+        ips = [ "10.197.16.4/20" "fd3d:b64f:631d:8284::4/64" ];
+        privateKeyFile =
+          config.age.secrets.wireguard-wallace-fediversity-private-key.path;
+        peers = [{
+          publicKey = "TKTBW6RUjsMc9I9bH31vBUMZZByZVhL7rDENHjEcgyw=";
+          allowedIPs =
+            [ "10.197.16.0/20" "192.168.51.0/24" "fd3d:b64f:631d:8284::/64" ];
+          endpoint = "vpn.fediversity.eu:51820";
+        }];
+      };
+    };
+  };
+  networking.firewall.allowedTCPPorts = [ 51820 ];
+  networking.firewall.allowedUDPPorts = [ 51820 ];
+  age.secrets.wireguard-wallace-fediversity-private-key.file =
+    "${secrets}/wireguard-wallace-fediversity-private-key.age";
 }
