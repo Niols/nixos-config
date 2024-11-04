d4e4bb5233ba4a39bdd06b8bb338097c8b47aafd
Niols <niols@niols.fr>
2024-11-04 13:39:39 +0100

    Remove uselessly opened port

--- wallace/network.nix
+++ wallace/network.nix
@@ -39,8 +39,7 @@ in {
       };
     };
   };
-  networking.firewall.allowedTCPPorts = [ 51820 ];
-  networking.firewall.allowedUDPPorts = [ 51820 ];
+
   age.secrets.wireguard-wallace-fediversity-private-key.file =
     "${secrets}/wireguard-wallace-fediversity-private-key.age";
 }
