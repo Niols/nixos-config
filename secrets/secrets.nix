f3ddd67997b3c9ab00d66570e089efb9314e9cd6
Niols <niols@niols.fr>
2025-05-13 18:08:34 +0200

    Add backups for Jellyfin

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -56,6 +56,8 @@ concatMapAttrs
       hester-firefly-iii-backup-identity = [ siegfried ];
       hester-git-backup-repokey = [ siegfried ];
       hester-git-backup-identity = [ siegfried ];
+      hester-jellyfin-backup-repokey = [ orianne ];
+      hester-jellyfin-backup-identity = [ orianne ];
       hester-teamspeak-backup-repokey = [ helga ];
       hester-teamspeak-backup-identity = [ helga ];
       hester-web-backup-repokey = [ helga ];
