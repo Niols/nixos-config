1d7ae3c2f2f9f1e79c05725f94205ae3296e8390
Niols <niols@niols.fr>
2025-06-18 17:55:56 +0200

    Factorise packages

--- _common/default.nix
+++ _common/default.nix
@@ -19,10 +19,10 @@ in
 
     ./hester.nix
     ./nix.nix
-    ./packages.nix
     ./syncthing.nix
 
     ./both/boot.nix
+    ./both/packages.nix
 
     ./server/autoreboot.nix
     ./server/databases.nix
@@ -39,7 +39,9 @@ in
     ./syncthing.nix
 
     ./both/boot.nix
+    ./both/packages.nix
 
+    ./laptop/packages
     ./laptop/users.nix
   ];
 }
