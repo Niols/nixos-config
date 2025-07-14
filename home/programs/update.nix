4d1d896d386de84b8f590f420f985ac8721afd93
Niols <niols@niols.fr>
2025-07-14 18:20:47 +0100

    Make update script tag current gen and reboot

--- home/programs/update.nix
+++ home/programs/update.nix
@@ -28,8 +28,30 @@
 
         printf 'done.\nUpgrading NixOS configuration...\n'
         sudo true
-        sudo nixos-rebuild switch --builders '@/etc/nix/machines' |& nom
-        printf 'done.\n'
+        sudo nixos-rebuild boot --builders '@/etc/nix/machines' |& nom
+
+        printf 'done.\nAdding a Git tag for the current generation...\n'
+        output=$(nixos-rebuild list-generations --json | jq '.[] | select(.current == true)')
+        if [ -z "$output" ]; then
+          printf '\e[31mError: no current generation found.\n\e[0m'
+          exit 2
+        fi
+        generation=$(echo "$output" | jq -r .generation)
+        date=$(echo "$output" | jq -r .date | cut -d ' ' -f 1)
+        nixosVersion=$(echo "$output" | jq -r .nixosVersion)
+        git tag "$generation" -m "NixOS - Configuration $generation ($date - $nixosVersion)"
+
+        printf 'done.\nPushing changes to remote...\n'
+        git push --tags
+
+        printf 'done.\nDo you wish to reboot? (y/N) '
+        read -r answer
+        if [[ "$answer" == [yY] || "$answer" == [yY][eE][sS] ]]; then
+          printf 'Rebooting...\n'
+          reboot
+        else
+          printf 'done.\n'
+        fi
       '';
     })
   ];
