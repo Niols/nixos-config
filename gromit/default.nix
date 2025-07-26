f6efc07a9d95d78d2b4e66f594462718a197181d
Niols <niols@niols.fr>
2025-07-26 17:42:29 +0200

    clean up for now

--- gromit/default.nix
+++ gromit/default.nix
@@ -1,4 +1,4 @@
-{ self, inputs, ... }:
+{ inputs, ... }:
 
 {
   flake.nixosModules.gromit = {
@@ -10,45 +10,45 @@
       ## Specific hardware optimisations for Lenovo ThinkPad X1 9th gen
       inputs.nixos-hardware.nixosModules.lenovo-thinkpad-x1-9th-gen
 
-      inputs.agenix.nixosModules.default
-      {
-        _module.args = {
-          inherit (inputs) nixpkgs;
-        };
-      }
+      # inputs.agenix.nixosModules.default
+      # {
+      #   _module.args = {
+      #     inherit (inputs) nixpkgs;
+      #   };
+      # }
 
       inputs.disko.nixosModules.disko
 
-      self.nixosModules.keys
-      self.nixosModules.secrets
-      { x_niols.hostPublicKey = self.keys.machines.wallace; }
+      # self.nixosModules.keys
+      # self.nixosModules.secrets
+      # { x_niols.hostPublicKey = self.keys.machines.gromit; }
 
-      inputs.nix-index-database.nixosModules.nix-index
+      # inputs.nix-index-database.nixosModules.nix-index
 
-      inputs.home-manager.nixosModules.home-manager
-      {
-        home-manager = {
-          ## By default, Home Manager uses a private pkgs instance that is
-          ## configured via the `home-manager.users.<name>.nixpkgs` options.
-          ## The following option instead uses the global pkgs that is
-          ## configured via the system level nixpkgs options; This saves an
-          ## extra Nixpkgs evaluation, adds consistency, and removes the
-          ## dependency on `NIX_PATH`, which is otherwise used for importing
-          ## Nixpkgs.
-          useGlobalPkgs = true;
+      # inputs.home-manager.nixosModules.home-manager
+      # {
+      #   home-manager = {
+      #     ## By default, Home Manager uses a private pkgs instance that is
+      #     ## configured via the `home-manager.users.<name>.nixpkgs` options.
+      #     ## The following option instead uses the global pkgs that is
+      #     ## configured via the system level nixpkgs options; This saves an
+      #     ## extra Nixpkgs evaluation, adds consistency, and removes the
+      #     ## dependency on `NIX_PATH`, which is otherwise used for importing
+      #     ## Nixpkgs.
+      #     useGlobalPkgs = true;
 
-          ## By default packages will be installed to `$HOME/.nix-profile` but
-          ## they can be installed to `/etc/profiles` if the following is
-          ## added to the system configuration. This option may become the
-          ## default value in the future.
-          useUserPackages = true;
+      #     ## By default packages will be installed to `$HOME/.nix-profile` but
+      #     ## they can be installed to `/etc/profiles` if the following is
+      #     ## added to the system configuration. This option may become the
+      #     ## default value in the future.
+      #     useUserPackages = true;
 
-          users = {
-            niols = import ../home { inherit inputs; };
-            root = import ../home { inherit inputs; };
-          };
-        };
-      }
+      #     users = {
+      #       niols = import ../home { inherit inputs; };
+      #       root = import ../home { inherit inputs; };
+      #     };
+      #   };
+      # }
     ];
   };
 }
