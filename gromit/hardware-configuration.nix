39b4d1869000fcab1890c5ac3a4ffe5ab86c88f4
Niols <niols@niols.fr>
2025-07-27 16:41:54 +0200

    more option

--- gromit/hardware-configuration.nix
+++ gromit/hardware-configuration.nix
@@ -3,11 +3,13 @@
 {
   nixpkgs.hostPlatform = "x86_64-linux";
 
+  boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.grub.enable = true;
   boot.loader.grub.efiSupport = true;
   boot.loader.grub.enableCryptodisk = true;
   boot.loader.grub.efiInstallAsRemovable = true;
   boot.loader.grub.device = "nodev"; # Add this line - don't install to MBR
+  boot.loader.grub.useOSProber = false; # Disable OS probing
 
   boot.tmp.useTmpfs = true;
   boot.tmp.cleanOnBoot = true;
