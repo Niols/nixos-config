bf92c0f49539553b003a61628a9da0c461a32af9
Niols <niols@niols.fr>
2025-09-09 18:07:45 +0200

    Make Wallace's configuration look more like a module

--- wallace/default.nix
+++ wallace/default.nix
@@ -2,34 +2,30 @@
 
 {
   flake.nixosModules.wallace = {
+    _module.args = {
+      inherit (inputs) nixpkgs;
+    };
+
     imports = [
       (import ../_common).laptop
       ../_modules/niols-motd.nix
 
-      ./hardware-configuration.nix
-      ./network.nix
-      ./storage.nix
+      self.nixosModules.keys
+      self.nixosModules.secrets
 
-      ./legacy-configuration.nix
-      ./syncthing.nix
-
+      inputs.agenix.nixosModules.default
+      inputs.disko.nixosModules.disko
+      inputs.nix-index-database.nixosModules.nix-index
+      inputs.home-manager.nixosModules.home-manager
       ## Specific hardware optimisations for Lenovo ThinkPad X1 9th gen
       inputs.nixos-hardware.nixosModules.lenovo-thinkpad-x1-9th-gen
 
-      inputs.agenix.nixosModules.default
-      {
-        _module.args = {
-          inherit (inputs) nixpkgs;
-        };
-      }
+      ./hardware-configuration.nix
+      ./network.nix
+      ./storage.nix
+      ./legacy-configuration.nix
+      ./syncthing.nix
 
-      self.nixosModules.keys
-      self.nixosModules.secrets
-      { x_niols.hostPublicKey = self.keys.machines.wallace; }
-
-      inputs.nix-index-database.nixosModules.nix-index
-
-      inputs.home-manager.nixosModules.home-manager
       {
         home-manager = {
           ## By default, Home Manager uses a private pkgs instance that is
@@ -54,6 +50,8 @@
         };
       }
     ];
+
+    x_niols.hostPublicKey = self.keys.machines.wallace;
 
     services.picom.enable = true;
     services.autorandr.x_niols.thisLaptopsFingerprint = "00ffffffffffff000e6f031400000000001e0104b51e1378032594af5042b0250d4e550000000101010101010101010101010101010180e800a0f0605090302036002ebd10000018000000fd00303c95953c010a202020202020000000fe0043534f542054330a2020202020000000fe004d4e453030375a41312d320a2001a102030f00e3058000e60605016a6a24000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000009a";
