fc7f6dd2434afae91f06c0134747a3ae1f854514
Niols <niols@niols.fr>
2025-09-10 10:49:00 +0200

    [WIP] More Gromit

--- gromit/default.nix
+++ gromit/default.nix
@@ -1,62 +1,51 @@
-{ inputs, ... }:
+{ self, inputs, ... }:
 
 {
   flake.nixosModules.gromit = {
-    networking.hostName = "gromit";
+    _module.args = {
+      inherit (inputs) nixpkgs;
+    };
 
     imports = [
       (import ../_common).laptop
+      ../_modules/niols-motd.nix
 
-      ./hardware-configuration.nix
+      self.nixosModules.keys
+      self.nixosModules.secrets
 
+      inputs.agenix.nixosModules.default
+      inputs.disko.nixosModules.disko
+      inputs.nix-index-database.nixosModules.nix-index
+      inputs.home-manager.nixosModules.home-manager
       ## Specific hardware optimisations for Lenovo ThinkPad X1 9th gen
       inputs.nixos-hardware.nixosModules.lenovo-thinkpad-x1-9th-gen
 
-      # inputs.agenix.nixosModules.default
-      # {
-      #   _module.args = {
-      #     inherit (inputs) nixpkgs;
-      #   };
-      # }
-
-      inputs.disko.nixosModules.disko
-
-      {
-        _module.args = {
-          inherit (inputs) nixpkgs;
-        };
-      }
-
-      # self.nixosModules.keys
-      # self.nixosModules.secrets
-      # { x_niols.hostPublicKey = self.keys.machines.gromit; }
-
-      # inputs.nix-index-database.nixosModules.nix-index
-
-      # inputs.home-manager.nixosModules.home-manager
-      # {
-      #   home-manager = {
-      #     ## By default, Home Manager uses a private pkgs instance that is
-      #     ## configured via the `home-manager.users.<name>.nixpkgs` options.
-      #     ## The following option instead uses the global pkgs that is
-      #     ## configured via the system level nixpkgs options; This saves an
-      #     ## extra Nixpkgs evaluation, adds consistency, and removes the
-      #     ## dependency on `NIX_PATH`, which is otherwise used for importing
-      #     ## Nixpkgs.
-      #     useGlobalPkgs = true;
-
-      #     ## By default packages will be installed to `$HOME/.nix-profile` but
-      #     ## they can be installed to `/etc/profiles` if the following is
-      #     ## added to the system configuration. This option may become the
-      #     ## default value in the future.
-      #     useUserPackages = true;
-
-      #     users = {
-      #       niols = import ../home { inherit inputs; };
-      #       root = import ../home { inherit inputs; };
-      #     };
-      #   };
-      # }
+      ./network.nix
+      ./legacy-configuration.nix
+      ./syncthing.nix
     ];
+
+    x_niols.hostPublicKey = self.keys.machines.gromit;
+
+    nixpkgs.hostPlatform = "x86_64-linux";
+
+    services.autorandr.x_niols.thisLaptopsFingerprint = "FIXME";
+
+    niols-motd = {
+      enable = true;
+      hostname = "Gromit";
+      hostcolour = "green";
+    };
+
+    programs.weylus = {
+      enable = true;
+      openFirewall = true;
+      users = [ "niols" ];
+    };
+
+    home-manager.users = {
+      niols = import ../home { inherit inputs; };
+      root = import ../home { inherit inputs; };
+    };
   };
 }
