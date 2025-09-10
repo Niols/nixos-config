a3b495766cf3724080f366591ea1e50cd002b381
Niols <niols@niols.fr>
2025-09-10 12:17:27 +0200

    Optional unencrypted `/opt` partition

--- _common/laptop/hardware.nix
+++ _common/laptop/hardware.nix
@@ -10,14 +10,27 @@ let
 
 in
 {
-  options.x_niols.enableDiskoConfig = mkOption {
-    description = ''
-      Whether to use Disko for the definition of disks. This is enabled by
-      default and should only be disabled for legacy devices, installed before
-      Disko was a thing in this configuration.
-    '';
-    type = types.bool;
-    default = true;
+  options.x_niols = {
+    enableDiskoConfig = mkOption {
+      description = ''
+        Whether to use Disko for the definition of disks. This is enabled by
+        default and should only be disabled for legacy devices, installed before
+        Disko was a thing in this configuration.
+      '';
+      type = types.bool;
+      default = true;
+    };
+
+    unencryptedOptSize = mkOption {
+      description = ''
+        The size of the unencrypted `/opt` partition, or `null` to not have one.
+        This partition can be useful for video games, where security matters
+        less, but IO performances matter more.
+      '';
+      example = "200GB";
+      type = with types; nullOr str;
+      default = null;
+    };
   };
 
   config = mkMerge [
@@ -48,7 +61,6 @@ in
               ## NOTE: LUKS passphrase will be prompted interactively only.
               ##
               luks = {
-                size = "100%";
                 content = {
                   type = "luks";
                   name = "crypted";
@@ -58,8 +70,29 @@ in
                     vg = "pool";
                   };
                 };
-              };
-            };
+              }
+              // (
+                if config.x_niols.unencryptedOptSize == null then
+                  { size = "100%"; }
+                else
+                  { end = "-${config.x_niols.unencryptedOptSize}"; }
+              );
+            }
+            // (
+              if config.x_niols.unencryptedOptSize == null then
+                { }
+              else
+                {
+                  opt = {
+                    size = "100%";
+                    content = {
+                      type = "filesystem";
+                      format = "ext4";
+                      mountpoint = "/opt";
+                    };
+                  };
+                }
+            );
           };
         };
 
