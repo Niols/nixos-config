2809b2704ead84b5f743c4d467a892df5d5db9b3
Niols <niols@niols.fr>
2025-09-15 09:32:52 +0200

    Gather imported files automatically

--- _common/default.nix
+++ _common/default.nix
@@ -1,49 +1,11 @@
 ## NOTE: Depending on the type of machine, we might not want to include exactly
 ## the same modules.
 
+let
+  inherit (builtins) readDir attrNames map;
+  allFilesFrom = dir: map (fname: dir + "/${fname}") (attrNames (readDir dir));
+in
 {
-  server.imports = [
-    ./both/constants.nix
-    ./both/hester.nix
-    ./both/home-manager.nix
-    ./both/network.nix
-    ./both/nix.nix
-    ./both/packages.nix
-    ./both/ssh.nix
-    ./both/syncthing.nix
-    ./both/systemStateVersion.nix
-
-    ./server/autoreboot.nix
-    ./server/boot.nix
-    ./server/databases.nix
-    ./server/home-manager.nix
-    ./server/nix.nix
-    ./server/ssh.nix
-    ./server/users.nix
-  ];
-
-  laptop.imports = [
-    ./both/constants.nix
-    ./both/hester.nix
-    ./both/home-manager.nix
-    ./both/network.nix
-    ./both/nix.nix
-    ./both/packages.nix
-    ./both/ssh.nix
-    ./both/syncthing.nix
-    ./both/systemStateVersion.nix
-
-    ./laptop/autorandr.nix
-    ./laptop/boot.nix
-    ./laptop/hardware.nix
-    ./laptop/network.nix
-    ./laptop/nix.nix
-    ./laptop/packages
-    ./laptop/syncthing.nix
-    ./laptop/timezone.nix
-    ./laptop/udev.nix
-    ./laptop/users.nix
-    ./laptop/xserver.nix
-    ./laptop/zzz.nix
-  ];
+  server.imports = allFilesFrom ./both ++ allFilesFrom ./server;
+  laptop.imports = allFilesFrom ./both ++ allFilesFrom ./laptop;
 }
