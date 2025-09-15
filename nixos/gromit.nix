a69c1895e7122369c3d494cdd18a2cea433bc6f6
Niols <niols@niols.fr>
2025-09-15 16:14:23 +0200

    Move Gromit and Wallace's configurations to single files

--- nixos/gromit.nix
+++ nixos/gromit.nix
@@ -5,8 +5,8 @@
     { config, inputs, ... }:
     {
       imports = [
-        ../_common/laptop.nix
-        ../../_modules/niols-motd.nix
+        _common/laptop.nix
+        ../_modules/niols-motd.nix
 
         ## Specific hardware optimisations for Lenovo ThinkPad X1 9th gen
         inputs.nixos-hardware.nixosModules.lenovo-thinkpad-x1-9th-gen
@@ -21,6 +21,6 @@
       services.autorandr.x_niols.thisLaptopsFingerprint = "00ffffffffffff000e6f031400000000001e0104b51e137803249daf5043b0250e4f540000000101010101010101010101010101010180e800a0f0605090302036002ebd10000018000000fd00303c95953c010a202020202020000000fe0043534f542054330a2020202020000000fe004d4e453030375a41312d320a20019702030f00e3058000e60605016a6a24000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000009a";
 
       home-manager.extraSpecialArgs = { inherit inputs; };
-      home-manager.users.niols.imports = [ ../../home/laptop-niols.nix ];
+      home-manager.users.niols.imports = [ ../home/laptop-niols.nix ];
     };
 }
