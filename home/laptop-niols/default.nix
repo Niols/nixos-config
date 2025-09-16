4e8f2dde7a85fbd881da2c08db396aa114150149
Niols <niols@niols.fr>
2025-09-16 16:21:10 +0200

    Move a bunch of packages to have them `niols`-specific

--- home/laptop-niols/default.nix
+++ home/laptop-niols/default.nix
@@ -1,3 +1,5 @@
+{ pkgs, ... }:
+
 {
   imports = [ ../_common/laptop ];
 
@@ -5,4 +7,22 @@
   home.homeDirectory = "/home/niols";
 
   home.file.".face".source = ./face.jpg;
+
+  ## Packages that are only ever used on my personal laptops. They should not
+  ## clutter work's environment, (and that eliminates the temptation to have
+  ## Signal or Thunderbird running)!
+  home.packages = with pkgs; [
+    audacity
+    element-desktop
+    gnucash
+    inkscape
+    ledger-live-desktop
+    libreoffice
+    lilypond
+    picard
+    signal-desktop
+    thunderbird
+    nextcloud-client
+    vlc
+  ];
 }
--- nixos/_common/laptop/packages/system.nix
+++ nixos/_common/laptop/packages/system.nix
@@ -5,13 +5,11 @@ with pkgs;
   arandr # REVIEW: probably not needed now that we have autorandr
   ardour
   asunder
-  audacity
 
   ## C
   ## caffeine-ng -> the behaviour is really inconsistent
 
   ## E
-  element-desktop
   evince
 
   ## F
@@ -23,22 +21,15 @@ with pkgs;
   ## G
   ghostscript # for `gs`, which `imagemagick` uses for PDF manipulation
   gimp
-  gnucash
   gnupg
   guile # REVIEW: is it needed? maybe for LilyPond?
   guvcview # REVIEW: what is this for?
 
   ## I
-  inkscape
   imagemagick
 
   ## K
   keepassxc
-
-  ## L
-  ledger-live-desktop # Wallet app for Ledger devices
-  libreoffice
-  lilypond
 
   ## M
   mosh
@@ -46,27 +37,22 @@ with pkgs;
   ## N
   nautilus
   nautilus-open-any-terminal
-  nextcloud-client
 
   ## P
   pdfgrep
   pdftk
-  picard
   pkg-config # REVIEW: remove, move to `both`, or move to a new `dev` packages module
 
   ## S
   scrcpy # REVIEW: remove, move to `both`, or move to a new `dev` packages module
-  signal-desktop
   steam-run
 
   ## T
   texlive.combined.scheme-full
-  thunderbird
   tmate
 
   ## V
   vdhcoapp # companion for Video DownloadHelper
-  vlc
 
   ## X
   xf86_input_wacom # wacom tablet support + `xsetwacom`
