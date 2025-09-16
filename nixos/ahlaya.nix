067e883a295f2c0f55e34226a9543f71bbd6a66a
Niols <niols@niols.fr>
2025-09-16 10:29:07 +0200

    [WIP] Ahrefs VPN

--- nixos/ahlaya.nix
+++ nixos/ahlaya.nix
@@ -23,5 +23,65 @@
       home-manager.extraSpecialArgs = { inherit inputs; };
       home-manager.users.niols.imports = [ ../home/laptop-niols.nix ];
       home-manager.users.work.imports = [ ../home/laptop-work.nix ];
+
+      ##############################################################################
+      ## Ahrefs VPN
+
+      networking.wireguard = {
+        enable = true;
+        interfaces = {
+          ahrefs = {
+            ips = [
+              "192.168.45.67/32"
+              "fd86:0:45::67/128"
+            ];
+            privateKeyFile = config.age.secrets.wireguard-ahlaya-ahrefs-key.path;
+            peers = [
+              {
+                publicKey = "J4JjnCIuqMIEKcS98w1OyZnTiSlVQzUTrz8BhV7N3F8=";
+                allowedIPs = [
+                  "192.168.45.1"
+                  "fd86:0:45::1"
+                  "5.188.12.0/22"
+                  "3.120.100.39"
+                  "13.248.206.194"
+                  "76.223.89.128"
+                  "15.197.162.230"
+                  "3.33.179.158"
+                  "13.248.243.217"
+                  "76.223.105.174"
+                  "61.16.24.0/22"
+                  "103.150.140.0/23"
+                  "168.100.128.0/19"
+                  "202.8.40.0/22"
+                  "202.94.84.0/23"
+                  "2001:470:24a::/48"
+                  "2001:470:3b8::/48"
+                  "2001:470:3bb::/48"
+                  "2001:df3:7c80::/48"
+                  "2401:59a0::/32"
+                  "104.18.39.141"
+                  "162.159.140.4"
+                  "172.64.148.115"
+                  "172.66.0.4"
+                  "2606:4700:4404::ac40:9473"
+                  "2606:4700:7::4"
+                  "2a06:98c1:3106::6812:278d"
+                  "2a06:98c1:3122:8000::"
+                  "2a06:98c1:3123:8000::"
+                  "2a06:98c1:58::4"
+                  "8.47.69.0"
+                  "8.6.112.0"
+                  "18.193.164.59"
+                  "44.210.147.40"
+                ];
+                endpoint = "backend-vpn.ahrefs.net:4433";
+              }
+            ];
+          };
+        };
+      };
+      networking.firewall.allowedTCPPorts = [ ];
+      networking.firewall.allowedUDPPorts = [ ];
     };
 }
