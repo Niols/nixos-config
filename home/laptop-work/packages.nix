db3d0cbd4b8b95b3e813bce5d3cbb41a338b8d2c
Niols <niols@niols.fr>
2025-09-17 10:24:53 +0200

    Split work packages - add GNU Make

---
+++ home/laptop-work/packages.nix
@@ -0,0 +1,22 @@
+{ pkgs, ... }:
+
+{
+  ## FIXME: Some things like this would deserve to be shared between `nixos/`
+  ## and `home/`, so probably we need something `_common` at the root too?
+  ##
+  ## FIXME: We shouldn't be setting things in `nixpkgs` because we are using
+  ## `useGlobalPkgs` in the NixOS configurations. Figure it out.
+  ##
+  nixpkgs.config.allowUnfreePredicate =
+    pkg:
+    builtins.elem (pkgs.lib.getName pkg) [
+      "slack"
+      "zoom"
+    ];
+
+  home.packages = with pkgs; [
+    gnumake
+    slack
+    zoom-us
+  ];
+}
