022b92084e20ecbbd63c4cf779ea1923249409bd
Niols <niols@niols.fr>
2025-09-17 10:38:16 +0200

    Set up Ahrefs's SSH

--- home/laptop-work/default.nix
+++ home/laptop-work/default.nix
@@ -2,15 +2,11 @@
   imports = [
     ../_common/laptop
     ./packages.nix
+    ./ssh.nix
   ];
 
   home.username = "work";
   home.homeDirectory = "/home/work";
 
   home.file.".face".source = ./face.jpg;
-
-  programs.ssh.matchBlocks."*" = {
-    identitiesOnly = true;
-    identityFile = "~/.ssh/id_ahrefs";
-  };
 }
