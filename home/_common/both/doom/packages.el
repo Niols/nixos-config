3cff0c99a333f04de6506b36bb6715225a3ef97d
Niols <niols@niols.fr>
2025-09-19 12:48:16 +0200

    Exit gitea

--- home/_common/both/doom/packages.el
+++ home/_common/both/doom/packages.el
@@ -4,9 +4,6 @@
 (package! copilot
   :recipe (:host github :repo "copilot-emacs/copilot.el" :files ("*.el" "dist")))
 
-(package! gitea-forge
-  :recipe (:host github :repo "mobid/gitea-forge"))
-
 (package! lilypond-mode
   :recipe (:host gitlab
            :repo "lilypond/lilypond"
