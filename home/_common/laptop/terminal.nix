8945174338277c210a6fc46bb6bc09b498e34f6c
Niols <niols@niols.fr>
2025-09-19 11:51:37 +0200

    Configure everything about the terminal emulator in one place

---
+++ home/_common/laptop/terminal.nix
@@ -0,0 +1,28 @@
+{ config, lib, ... }:
+
+let
+  inherit (lib) mkOption types;
+
+in
+
+{
+  options.x_niols.terminalCommand = mkOption {
+    description = ''
+      Command to run the terminal emulator. This option is meant to be used in
+      other places in the configuration, so as to avoid hardcoding the terminal
+      emulator.
+    '';
+    type = types.str;
+    default = "xfce4-terminal";
+  };
+
+  config = {
+    xfconf.settings.xfce4-terminal = {
+      background-mode = "TERMINAL_BACKGROUND_IMAGE";
+      background-image-file = config.x_niols.backgroundImageFile;
+      background-image-style = "TERMINAL_BACKGROUND_STYLE_CENTERED";
+      background-darkness = 0.6;
+      background-image-shading = 0.6;
+    };
+  };
+}
