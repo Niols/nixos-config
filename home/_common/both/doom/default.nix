d99f4c3be6a49b6f46babee1e3cbce93ce6a8611
Niols <niols@niols.fr>
2025-09-19 17:23:17 +0200

    [WIP] Better Doom Emacs

--- home/_common/both/doom/default.nix
+++ home/_common/both/doom/default.nix
@@ -5,26 +5,14 @@
   ...
 }:
 
-# let
-#   doom = pkgs.runCommand "doom" {
-#     buildInputs = with pkgs; [ emacs ];
-#   } ''
-#     echo DOOOOOM
-#     set +x
-#     cp -R ${inputs.doomemacs} emacs
-#     chmod -R u+w emacs
-#     emacs/bin/doom install --force --install --doomdir ./.
-#     emacs/bin/doom sync --force --doomdir ./.
-#     mv emacs $out
-#   '';
-
-#   in
 {
-  programs.emacs.enable = true;
+  ## Make sure that Home Manager is disabled for Emacs, we will handle things ourselves.
+  programs.emacs.enable = false;
 
   home.packages = (
     with pkgs;
     [
+      emacs
       cmake # necessary for Emacs's `vterm`
       libtool # necessary for Emacs's `vterm`
       nerd-fonts.symbols-only
@@ -40,30 +28,74 @@
     ]
   );
 
-  home.file.".config/doom/config.el".source = ./config.el;
-  home.file.".config/doom/custom.el".source = ./custom.el;
-  home.file.".config/doom/init.el".source = ./init.el;
-  home.file.".config/doom/packages.el".source = ./packages.el;
-
-  ## FIXME: Detect when Doom has not been updated.
-  ## FIXME: Detect when ~/.config/emacs is not under our control.
   home.activation.doom = lib.hm.dag.entryAfter [ "linkGeneration" ] ''
-    # rm -Rf "$HOME"/.config/emacs
-    # cp -R ''${doom} "$HOME"/.config/emacs
-    # chmod -R u+w "$HOME/.config/emacs
+    set -xeuC
 
-    echo Bumping Emacs folder
-    rm -Rf "$HOME"/.config/emacs
-    cp -R ${inputs.doomemacs} "$HOME"/.config/emacs
-    chmod -R u+w "$HOME"/.config/emacs
-    echo Instaling Doom
-    "$HOME"/.config/emacs/bin/doom install --force --install
-    echo Syncing Doom with config
-    "$HOME"/.config/emacs/bin/doom sync --force
+    readonly emacsdir=$HOME/.config/emacs
+    readonly emacslocaldir=$emacsdir/.local
+
+    readonly narhash=${inputs.doomemacs.narHash}
+    readonly narhashfile=$emacslocaldir/narhashfile
+    readonly lastmodified=${toString inputs.doomemacs.lastModified}
+    readonly lastmodifiedfile=$emacslocaldir/lastmodified
+
+    ## For increased reproducibility, always run doom as if from the exact time
+    ## where the lastModified commit is from. NOTE: We do not use the flake
+    ## `-f`, such that we allow time to pass. This gives less reproducibility,
+    ## but otherwise the compilation of company-math hangs, somehow.
+    doom () {
+      ${pkgs.libfaketime}/bin/faketime "$(date --date="@$lastmodified" +'%F %T')" \
+        "$emacsdir"/bin/doom --emacsdir "$emacsdir" --doomdir ${./.} "$@"
+    }
+
+    ## A temporary directory to move (parts of) the .local directory to.
+    readonly tmplocaldir=$(mktemp -d)
+
+    ## A utility to save a file from $emacslocaldir into $tmplocaldir.
+    save_local_file () {
+      if [ -e "$emacslocaldir"/"$1" ]; then
+        mkdir -p "$(dirname "$tmplocaldir"/"$1")"
+        cp -R "$emacslocaldir"/"$1" "$tmplocaldir"/"$1"
+      fi
+    }
+
+    ## Look at the current installation and detect whether we should
+    ## (re-)install Doom Emacs. In the case of a re-installation, save the parts
+    ## of the .local directory that we care about.
+    ##
+    if ! [ -e "$emacsdir" ]; then
+      echo "$emacsdir does not exist; we will proceed with a fresh installation."
+      must_install=true
+    elif ! [ -e "$narhashfile" ] || ! [ -e "$lastmodifiedfile" ] || [ "$(cat "$narhashfile")" != "$narHash" ] || [ "$(cat "$lastmodifiedfile")" != "$lastmodified" ]; then
+      echo "$emacsdir exists, but does not match that of the flake; we will proceed with a re-installation."
+      must_install=true
+      save_local_file etc
+      save_local_file cache/projectile/projects.eld
+    else
+      echo "$emacsdir exists and Doom Emacs has not been updated in the flake; we will only synchronise the configuration."
+      must_install=false
+    fi
+
+    if $must_install; then
+      echo "(Re-)installing Doom Emacs..."
+      rm -Rf "$emacsdir"
+      cp -R ${inputs.doomemacs} "$emacsdir"
+      chmod -R u+w "$emacsdir"
+      mv "$tmplocaldir" "$emacslocaldir"
+      echo "$narhash" > $narhashfile
+      echo "$lastmodified" > $lastmodifiedfile
+      doom install --force
+    fi
+
+    rm -Rf "$tmplocaldir"
+
+    echo "Syncing Doom Emacs's configuration..."
+    doom sync --force
   '';
 
   ## Enable true color/24-bit color support. This makes Emacs pretty in the
   ## terminal (otherwise it is okay-ish but not really usable). However, this
   ## might break some terminals that do not have support for it.
+  ## FIXME: Does not work.
   home.sessionVariables.TERM = "xterm-direct";
 }
