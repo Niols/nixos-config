e98aa5673a996a2e9ba57aed01e188366a6cb724
Niols <niols@niols.fr>
2025-09-20 10:22:01 +0200

    Make all home configurations grab `username` and `homeDirectory` from the environment by default

--- home/headless-work/default.nix
+++ home/headless-work/default.nix
@@ -1,7 +1,3 @@
 {
   imports = [ ../_common/headless.nix ];
-
-  ## TODO: Move to _common/both and make them as default
-  home.username = builtins.getEnv "USER";
-  home.homeDirectory = builtins.getEnv "HOME";
 }
