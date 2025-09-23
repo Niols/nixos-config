355c32648bb0923f30e44d2764e95dbf906cacdb
Niols <niols@niols.fr>
2025-09-23 15:28:11 +0200

    Clean up useless home packages, in particular gnupg and pinentry
    As for home packages like `rg`, we should bring them back, as well as
    many others, by sharing core packages between NixOS and home
    configurations

--- home/packages.nix
+++ home/packages.nix
@@ -11,15 +11,6 @@ let
 in
 {
   config = mkMerge [
-    {
-      home.packages = with pkgs; [
-        gnupg # ?
-        pinentry # ?
-        ripgrep # provides `rg`
-      ];
-      programs.lsd.enable = true;
-    }
-
     ## Packages that are only ever used on my personal laptops. They should not
     ## clutter work's environment, (and that eliminates the temptation to have
     ## Signal or Thunderbird running)!
