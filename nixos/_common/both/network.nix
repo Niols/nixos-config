72ef646e673e099f118e9dbe80bc1c1c89850698
Niols <niols@niols.fr>
2025-09-30 12:48:34 +0200

    Use Cloudflare and Google as DNS on all machines

--- nixos/_common/both/network.nix
+++ nixos/_common/both/network.nix
@@ -4,5 +4,12 @@
   networking = {
     hostName = config.x_niols.thisDevicesNameLower;
     domain = "niols.fr";
+
+    nameservers = [
+      "1.1.1.1"
+      "1.0.0.1" # Cloudflare
+      "8.8.8.8"
+      "8.8.4.4" # Google
+    ];
   };
 }
