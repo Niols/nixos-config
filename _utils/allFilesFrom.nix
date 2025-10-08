bcc588b741eedd241b233612306be2ae9f1ad8e7
Nicolas Jeannerod <niols@niols.fr>
2025-10-08 15:26:57 +0200

    Exit `allFilesFrom` - use a more module-y architecture (#302)

--- _utils/allFilesFrom.nix
+++
@@ -1,6 +0,0 @@
-## Small utility that receives a path and returns a list of paths of all files
-## and directories in the given path.
-let
-  inherit (builtins) readDir attrNames map;
-in
-dir: map (fname: dir + "/${fname}") (attrNames (readDir dir))
---
+++ nixos/_common/both/default.nix
@@ -0,0 +1,17 @@
+{
+  imports = [
+    ./autoreboot.nix
+    ./boot.nix
+    ./constants.nix
+    ./databases.nix
+    ./dns-server.nix
+    ./hester.nix
+    ./network.nix
+    ./nix-cache.nix
+    ./packages.nix
+    ./ssh.nix
+    ./syncthing.nix
+    ./systemStateVersion.nix
+    ./users.nix
+  ];
+}
