fab003b339637a1bfcdb9bae8c7df28d7bad18c8
Nicolas Jeannerod <niols@niols.fr>
2025-10-20 14:47:35 +0200

    Merge hardware configurations into servers' main files (#335)

--- nixos/helga/default.nix
+++ nixos/helga/default.nix
@@ -1,20 +1,42 @@
 {
-  flake.nixosModules.helga = {
-    imports = [
-      ../_common/server.nix
-      ./hardware-configuration.nix
-      ./nginx.nix
-    ];
+  flake.nixosModules.helga =
+    { modulesPath, ... }:
+    {
+      imports = [
+        (modulesPath + "/profiles/qemu-guest.nix")
+        ../_common/server.nix
+        ./nginx.nix
+      ];
 
-    x_niols.thisMachinesName = "helga";
-    x_niols.thisMachinesColour = "blue";
-    x_niols.enableNiolsUser = true;
+      x_niols.thisMachinesName = "helga";
+      x_niols.thisMachinesColour = "blue";
+      x_niols.enableNiolsUser = true;
 
-    ## FIXME: This is an experiment to improve responsiveness of the system
-    ## when Dancelor uses the Nix builds so intensely. It might however starve
-    ## the Nix builds, and in particular the `nixos-rebuild`. Hopefully,
-    ## though, since it come from NixOps4, that is not a problem.
-    nix.daemonCPUSchedPolicy = "idle";
-    nix.daemonIOSchedClass = "idle";
-  };
+      ## FIXME: This is an experiment to improve responsiveness of the system
+      ## when Dancelor uses the Nix builds so intensely. It might however starve
+      ## the Nix builds, and in particular the `nixos-rebuild`. Hopefully,
+      ## though, since it come from NixOps4, that is not a problem.
+      nix.daemonCPUSchedPolicy = "idle";
+      nix.daemonIOSchedClass = "idle";
+
+      nixpkgs.hostPlatform = "x86_64-linux";
+
+      boot = {
+        loader.grub.device = "/dev/sda";
+        initrd = {
+          availableKernelModules = [
+            "ata_piix"
+            "uhci_hcd"
+            "xen_blkfront"
+            "vmw_pvscsi"
+          ];
+          kernelModules = [ "nvme" ];
+        };
+      };
+
+      fileSystems."/" = {
+        device = "/dev/sda1";
+        fsType = "ext4";
+      };
+    };
 }
--- nixos/siegfried/default.nix
+++ nixos/siegfried/default.nix
@@ -1,13 +1,53 @@
 {
-  flake.nixosModules.siegfried = {
-    imports = [
-      ../_common/server.nix
+  flake.nixosModules.siegfried =
+    { modulesPath, ... }:
+    {
+      imports = [
+        (modulesPath + "/profiles/qemu-guest.nix")
+        ../_common/server.nix
+        ./nginx.nix
+      ];
 
-      ./hardware-configuration.nix
-      ./nginx.nix
-    ];
+      x_niols.thisMachinesName = "siegfried";
+      x_niols.thisMachinesColour = "yellow";
 
-    x_niols.thisMachinesName = "siegfried";
-    x_niols.thisMachinesColour = "yellow";
-  };
+      nixpkgs.hostPlatform = "x86_64-linux";
+
+      fileSystems = {
+        "/" = {
+          device = "/dev/sda1";
+          fsType = "ext4";
+        };
+
+        "/boot" = {
+          device = "/dev/disk/by-uuid/7BB1-F695";
+          fsType = "vfat";
+        };
+      };
+
+      swapDevices = [
+        {
+          device = "/var/lib/swapfile";
+          size = 4 * 1024;
+        }
+      ];
+
+      boot = {
+        loader.grub = {
+          efiSupport = true;
+          efiInstallAsRemovable = true;
+          device = "nodev";
+        };
+
+        initrd = {
+          availableKernelModules = [
+            "ata_piix"
+            "uhci_hcd"
+            "xen_blkfront"
+            "vmw_pvscsi"
+          ];
+          kernelModules = [ "nvme" ];
+        };
+      };
+    };
 }
