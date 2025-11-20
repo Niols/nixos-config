19b70cafbf29dcf81b59d09b0bbf81861c7dd23b
Niols <niols@niols.fr>
2025-11-20 01:11:53 +0100

    Bump Nextcloud to v32

--- nixos/_common/both/cloud.nix
+++ nixos/_common/both/cloud.nix
@@ -49,7 +49,7 @@ in
         ## Nextcloud to see whether it recommends further processing, such as
         ## database reindexing or conversion.
         ##
-        package = pkgs.nextcloud31;
+        package = pkgs.nextcloud32;
 
         inherit hostName;
         settings.trusted_domains = otherHostNames;
