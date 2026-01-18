4365a4e85f281039b9b5734bca17c68b59a34286
Niols <niols@niols.fr>
2026-01-18 16:27:23 +0100

    more dark themes

--- home/gtk.nix
+++ home/gtk.nix
@@ -14,6 +14,7 @@ in
     gtk = {
       enable = true;
 
+      theme.package = pkgs.gnome-themes-extra;
       theme.name = "Adwaita-dark";
       iconTheme.name = "Adwaita";
       # iconTheme.package = pkgs.adwaita-icon-theme;
@@ -25,10 +26,19 @@ in
         gtk-key-theme-name = "Emacs";
         gtk-application-prefer-dark-theme = true;
       };
-      #gtk4.extraConfig = {
-      #  # gtk-key-theme-name = "Emacs";
-      #  gtk-application-prefer-dark-theme = true;
-      #};
+      gtk4.extraConfig = {
+        # gtk-key-theme-name = "Emacs";
+        gtk-application-prefer-dark-theme = true;
+      };
+    };
+
+    xdg.configFile = {
+      "gtk-4.0/assets".source =
+        "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/assets";
+      "gtk-4.0/gtk.css".source =
+        "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk.css";
+      "gtk-4.0/gtk-dark.css".source =
+        "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk-dark.css";
     };
 
     ## Fix "error: GDBus.Error:org.freedesktop.DBus.Error.ServiceUnknown: The name ca.desrt.dconf was not provided by any .service files"
