e911317cef1c6799abf83ac9e354ccdb49a444b0
Niols <niols@niols.fr>
2026-01-28 11:10:43 +0100

    Fix Adwaita dark on Firefox

--- home/gtk.nix
+++ home/gtk.nix
@@ -11,6 +11,12 @@ let
 in
 {
   config = mkIf (!config.x_niols.isHeadless) {
+    ## GTK themes are very messy. We use the simple Adwaita in dark mode, but
+    ## somehow different applications will grab this setting from different
+    ## places; hence the many occurrences of Adwaita dark in this page.
+
+    home.sessionVariables.GTK_THEME = "Adwaita:dark";
+
     gtk = {
       enable = true;
 
@@ -40,14 +46,14 @@ in
       };
     };
 
-    xdg.configFile = {
-      "gtk-4.0/assets".source =
-        "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/assets";
-      "gtk-4.0/gtk.css".source =
-        "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk.css";
-      "gtk-4.0/gtk-dark.css".source =
-        "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk-dark.css";
-    };
+    # xdg.configFile = {
+    #   "gtk-4.0/assets".source =
+    #     "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/assets";
+    #   "gtk-4.0/gtk.css".source =
+    #     "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk.css";
+    #   "gtk-4.0/gtk-dark.css".source =
+    #     "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk-dark.css";
+    # };
 
     ## Fix "error: GDBus.Error:org.freedesktop.DBus.Error.ServiceUnknown: The name ca.desrt.dconf was not provided by any .service files"
     ## see https://github.com/nix-community/home-manager/issues/3113
