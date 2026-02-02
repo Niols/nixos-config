363b3fc70a895c8cc4c3139d8a595a3fb24c4bc4
Niols <niols@niols.fr>
2026-02-02 10:54:33 +0100

    only on x86_64-linux

--- tests/flake-part.nix
+++ tests/flake-part.nix
@@ -4,11 +4,18 @@
   _class = "flake";
 
   perSystem =
-    { pkgs, ... }:
+    { pkgs, system, ... }:
     {
-      checks.nixops4-deployment = import ./nixops4-deployment {
-        inherit (pkgs.testers) runNixOSTest;
-        inherit inputs;
-      };
+      ## Only run the test on `x86_64-linux`. NOTE: It would be cleaner to use
+      ## `lib.optionalAttrs` rather than provider a dummy derivation, but our CI
+      ## expects the same names in all `checks.*` attribute sets.
+      checks.nixops4-deployment =
+        if system == "x86_64-linux" then
+          import ./nixops4-deployment {
+            inherit (pkgs.testers) runNixOSTest;
+            inherit inputs;
+          }
+        else
+          pkgs.hello;
     };
 }
