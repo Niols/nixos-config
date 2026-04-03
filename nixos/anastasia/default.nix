282ff7284cb7f949ea3acc52debc6b89ba249331
Niols <niols@niols.fr>
2026-04-03 22:16:32 +0200

    cleanup

--- nixos/anastasia/default.nix
+++ nixos/anastasia/default.nix
@@ -13,23 +13,5 @@
       x_niols.enableNiolsUser = true;
 
       nixpkgs.hostPlatform = "x86_64-linux";
-
-      boot = {
-        loader.grub.device = "/dev/sda";
-        initrd = {
-          availableKernelModules = [
-            "ata_piix"
-            "uhci_hcd"
-            "xen_blkfront"
-            "vmw_pvscsi"
-          ];
-          kernelModules = [ "nvme" ];
-        };
-      };
-
-      fileSystems."/" = {
-        device = "/dev/sda1";
-        fsType = "ext4";
-      };
     };
 }
