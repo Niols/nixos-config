94df0358f42e21e63ea1a0ffe133c394b90a2e72
Niols <niols@niols.fr>
2026-04-16 00:12:08 +0200

    Add Prettier to the pre-commit hooks

--- flake.nix
+++ flake.nix
@@ -91,6 +91,7 @@
           pre-commit.settings.hooks = {
             nixfmt.enable = true;
             deadnix.enable = true;
+            prettier.enable = true;
           };
 
           devShells.default = pkgs.mkShell {
