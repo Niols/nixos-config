f31e994c222be83c1766e6db27247986ff71c132
Niols <niols@niols.fr>
2026-05-03 11:05:45 +0200

    Document Anastasia's ethernet ports

--- nixos/anastasia/default.nix
+++ nixos/anastasia/default.nix
@@ -13,5 +13,12 @@
       x_niols.enableNiolsUser = true;
 
       nixpkgs.hostPlatform = "x86_64-linux";
+
+      ## In lack of a better place, here are Anastasia's ethernet ports,
+      ## physically from left to right:
+      ##
+      ## | 1 | enp1s0 | 2.5G | e4:3a:6e:85:83:3f |
+      ## | 2 | enp2s0 |  10G | e4:3a:6e:85:83:40 |
+      ## | 3 | enp5s0 | 2.5G | e4:3a:6e:85:84:38 |
     };
 }
