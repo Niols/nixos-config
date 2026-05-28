44101d2719a32d0155f701b52cc431779dbadf03
Niols <niols@niols.fr>
2026-05-28 11:06:42 +0200

    Generate Git workflow statically with Nix
    - Avoids having to run a “discover” job which takes about a minute extra
    - Allows more granularity in the generation of the matrix
    - Allows more expressivity in the checks that are being done
    - Allows sharing bits more easily

---
+++ .github/workflows/flake-part.nix
@@ -0,0 +1,230 @@
+{ self, lib, ... }:
+
+let
+  inherit (lib)
+    attrNames
+    attrValues
+    concatMap
+    mapAttrs
+    optionalString
+    ;
+
+  setupSteps = [
+    {
+      name = "Check out repository";
+      uses = "actions/checkout@v6";
+    }
+    {
+      name = "Install Nix";
+      uses = "cachix/install-nix-action@v31";
+      "with".extra_nix_config = "access-tokens = github.com=\${{ secrets.GITHUB_TOKEN }}";
+    }
+    {
+      name = "Install Attic";
+      run = "nix profile add .#attic";
+    }
+    {
+      name = "Set up Attic cache";
+      uses = "ryanccn/attic-action@v0.5";
+      "with" = {
+        endpoint = "https://nix-cache.niols.fr/";
+        cache = "nixos-config";
+        token = "\${{ secrets.ATTIC_TOKEN }}";
+      };
+    }
+  ];
+
+  ## NOTE: Orianne is an ARM machine, but the GitHub runners are Intel
+  ## machines, so we detect that, install the emulation binaries for
+  ## QEMU and tell Nix to behave as an `aarch64-linux` machine.
+  ##
+  setupEmulationLayerStep = {
+    name = "Set up emulation layer if necessary";
+    run = ''
+      system=''${{ matrix.system }}
+      echo "system = $system" > nix-config
+      if [ $system != x86_64-linux ]; then
+        printf 'This configuration is a %s, for which we need to install QEMU emulation binaries.\n' "$system"
+        sudo apt-get update -y && sudo apt-get install -y qemu-user-static
+      fi
+    '';
+  };
+
+in
+{
+  perSystem =
+    { pkgs, ... }:
+    {
+      ## A pre-commit hook that generates the GitHub workflows from Nix code.
+      ## This will also run in CI (see `jobs.checks` below) to check that
+      ## everything is in sync.
+      ##
+      ## NOTE: We could generate it by iterating in Nix rather than calling `nix
+      ## eval` then processing with `for` + `jq`, but the latter will catch
+      ## changes without having to reload the `direnv` every time.
+      ##
+      pre-commit.settings.hooks = {
+        github-workflows = {
+          enable = true;
+          files = "\\.nix$";
+          pass_filenames = false;
+          entry = "${pkgs.writeShellScript "make-github-workflows" ''
+            W=$(
+              ${pkgs.nix}/bin/nix eval \
+                --extra-experimental-features 'nix-command flakes' \
+                --impure --raw --expr 'with builtins; toJSON (getFlake (toString ./.)).github-workflows'
+            ) || exit 7
+            for workflow in $(echo "$W" | ${pkgs.jq}/bin/jq -r 'keys[]'); do
+              echo "$W" | ${pkgs.jq}/bin/jq ".$workflow" > .github/workflows/''${workflow}.yml || exit 7
+            done
+          ''}";
+        };
+        prettier.excludes = [ ".github/workflows/.*\\.yml$" ];
+      };
+    };
+
+  flake.github-workflows.ci = {
+    name = "CI";
+
+    on = {
+      push = {
+        branches = [ "main" ];
+        tags = [ "*" ];
+      };
+      pull_request = { };
+    };
+
+    ## We specify a concurrency group with automated cancellation. This means that
+    ## other pushes on the same `github.ref` (eg. other pushes to the same pull
+    ## request) cancel previous occurrences of the CI.
+    concurrency = {
+      group = "\${{ github.workflow }}-\${{ github.ref }}";
+      cancel-in-progress = true;
+    };
+
+    jobs = {
+      summarise = {
+        name = "Summarise";
+        runs-on = "ubuntu-latest";
+        needs = [
+          "checks"
+          "homeConfigurations"
+          "nixosConfigurations"
+        ];
+        "if" = "always()";
+        steps = [
+          {
+            uses = "re-actors/alls-green@release/v1";
+            "with" = {
+              jobs = "\${{ toJSON(needs) }}";
+              allowed-skips = "checks, homeConfigurations, nixosConfigurations";
+            };
+          }
+        ];
+      };
+
+      homeConfigurations = {
+        name = "Home";
+        runs-on = "ubuntu-latest";
+        strategy = {
+          matrix.include = map (home: { inherit home; }) (attrNames self.homeConfigurations);
+          fail-fast = false;
+        };
+        steps = setupSteps ++ [
+          {
+            ## NOTE: We build the home configurations as impure because they get
+            ## their `home.username` and `home.homeDirectory` from the environment
+            ## when they are not used via the Home Manager NixOS module.
+            name = "Build Home configuration “\${{ matrix.home }}”";
+            run = "nix build .#homeConfigurations.\${{ matrix.home }}.activationPackage --impure --print-build-logs";
+          }
+        ];
+      };
+
+      nixosConfigurations = {
+        name = "NixOS";
+        runs-on = "ubuntu-latest";
+        strategy = {
+          matrix.include = attrValues (
+            mapAttrs (name: nixosConfiguration: {
+              nixos = name;
+              system = nixosConfiguration.pkgs.stdenv.hostPlatform.system;
+              xtraSpace = optionalString (!(nixosConfiguration.config.x_niols.isServer)) "extra-space"; # laptops have very big closures
+            }) self.nixosConfigurations
+          );
+          fail-fast = false;
+        };
+        steps = setupSteps ++ [
+          {
+            name = "Free some extra space";
+            "if" = "\${{ matrix.xtraSpace == 'extra-space' }}";
+            run = ''
+              echo 'Available storage before:'
+              sudo df -h
+              echo
+              sudo rm -rf /usr/share/dotnet
+              sudo rm -rf /usr/local/lib/android
+              sudo rm -rf /opt/ghc
+              sudo rm -rf /opt/hostedtoolcache/CodeQL
+              echo 'Available storage after:'
+              sudo df -h
+              echo
+            '';
+          }
+          setupEmulationLayerStep
+          {
+            name = "Build NixOS configuration “\${{ matrix.nixos }}”";
+            run = ''
+              export NIX_CONFIG=$(cat nix-config)
+              nix build .#nixosConfigurations.''${{ matrix.nixos }}.config.system.build.toplevel --print-build-logs
+            '';
+          }
+          {
+            name = "Remaining space";
+            run = ''
+              echo 'Available storage:'
+              sudo df -h
+            '';
+          }
+          {
+            name = "Deploy NixOps4 component “\${{ matrix.nixos }}” if it exists";
+            "if" = "\${{ github.ref == 'refs/heads/main' }}";
+            run = ''
+              if nix develop --command nixops4 members list 2>/dev/null | grep '^''${{ matrix.nixos }}$'; then
+                echo "''${{ secrets.DEPLOY_KEY }}" > deploy-key
+                chmod 600 deploy-key
+                nix develop --command ssh-agent bash -c '
+                  ssh-add deploy-key
+                  export NIX_CONFIG=$(cat nix-config)
+                  nixops4 apply ''${{ matrix.nixos }}
+                '
+              fi
+            '';
+          }
+        ];
+      };
+
+      checks = {
+        name = "Check";
+        runs-on = "ubuntu-latest";
+        strategy = {
+          matrix.include = concatMap (
+            system: map (check: { inherit system check; }) (attrNames self.checks.${system})
+          ) (attrNames self.checks);
+          fail-fast = false;
+        };
+        steps = setupSteps ++ [
+          setupEmulationLayerStep
+          {
+            name = "Run check “\${{ matrix.check }}”";
+            run = ''
+              export NIX_CONFIG=$(cat nix-config)
+              nix build .#checks.''${{ matrix.system }}.''${{ matrix.check }} --print-build-logs
+            '';
+          }
+        ];
+
+      };
+    };
+  };
+}
--- tests/flake-part.nix
+++ tests/flake-part.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ inputs, lib, ... }:
 
 {
   _class = "flake";
@@ -6,16 +6,12 @@
   perSystem =
     { pkgs, system, ... }:
     {
-      ## Only run the test on `x86_64-linux`. NOTE: It would be cleaner to use
-      ## `lib.optionalAttrs` rather than provider a dummy derivation, but our CI
-      ## expects the same names in all `checks.*` attribute sets.
-      checks.nixops-deployment =
-        if system == "x86_64-linux" then
-          import ./nixops-deployment {
-            inherit (pkgs.testers) runNixOSTest;
-            inherit inputs;
-          }
-        else
-          builtins.trace "checks.*.nixops-deployment is trivial on non-x86_64-linux systems" pkgs.hello;
+      ## Only run the test on `x86_64-linux`.
+      checks = lib.optionalAttrs (system == "x86_64-linux") {
+        nixops-deployment = import ./nixops-deployment {
+          inherit (pkgs.testers) runNixOSTest;
+          inherit inputs;
+        };
+      };
     };
 }
