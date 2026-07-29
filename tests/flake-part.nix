d20ca9db86be769aa333f7e4b780d7a79ec8c6cb
Niols <niols@niols.fr>
2026-07-30 00:06:31 +0200

    [wip]

--- tests/flake-part.nix
+++ tests/flake-part.nix
@@ -1,17 +1,39 @@
-{ inputs, lib, ... }:
+{
+  self,
+  inputs,
+  lib,
+  ...
+}:
 
+let
+  inherit (lib)
+    mkMerge
+    optionalAttrs
+    ;
+
+in
 {
   _class = "flake";
 
   perSystem =
     { pkgs, system, ... }:
     {
-      ## Only run the test on `x86_64-linux`.
-      checks = lib.optionalAttrs (system == "x86_64-linux") {
-        nixops-deployment = import ./nixops-deployment {
-          inherit (pkgs.testers) runNixOSTest;
-          inherit inputs;
-        };
-      };
+      checks = mkMerge [
+        ## The nixops-deployment test can technically run on all platforms, but that makes
+        ## `nix flake check` quite wasteful; we limit it to x86_64-linux here.
+        (optionalAttrs (system == "x86_64-linux") {
+          nixops-deployment = import ./nixops-deployment {
+            inherit (pkgs.testers) runNixOSTest;
+            inherit inputs;
+          };
+        })
+
+        (optionalAttrs (system == "x86_64-linux") {
+          nixos-helga = pkgs.testers.runNixOSTest {
+            imports = [ ./nixosVmTestWip.nix ];
+            _module.args = { inherit self inputs lib; };
+          };
+        })
+      ];
     };
 }
