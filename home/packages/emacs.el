4fa7082dccdaaf48a38645f925276726a025a280
Niols <niols@niols.fr>
2026-08-19 10:30:15 +0200

    [fixup] apheleia

--- home/packages/emacs.el
+++ home/packages/emacs.el
@@ -123,7 +123,7 @@
 	  (eglot-format (region-beginning) (region-end))
 	  (eglot-format-buffer))
       (when (y-or-n-p "No LSP server. Format with apheleia?")
-	(apheleia-format-buffer))))
+	(call-interactively #'apheleia-format-buffer))))
 
 (use-package general
   :ensure t
