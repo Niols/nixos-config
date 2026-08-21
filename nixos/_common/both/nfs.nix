1606b6ea3c96c9a85d25619fd3067d77ad79eef7
Niols <niols@niols.fr>
2026-08-21 13:46:26 +0200

    [fixup] NFS

--- nixos/_common/both/nfs.nix
+++ nixos/_common/both/nfs.nix
@@ -72,6 +72,7 @@ in
               "x-systemd.automount" # defer the actual mount until the path is first accessed
               "x-systemd.mount-timeout=10" # give up trying to mount after 10s
               "x-systemd.after=wireguard-niols.service" # keep WG up until this mount has been torn down
+              "x-systemd.after=wireguard-niols-peer-${config.x_niols.services.nfs.enabledOn}.service"
             ];
           };
         }) datasetMountpoints
